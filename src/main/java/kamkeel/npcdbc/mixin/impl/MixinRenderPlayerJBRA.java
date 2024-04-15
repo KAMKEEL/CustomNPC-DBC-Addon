dbb414da2d60f4f525533da34a31b74e45a176b0
bigguy345 <monkeybar3200@gmail.com>
2024-04-15 10:19:37 +0200

    push pop pop push

--- src/main/java/kamkeel/npcdbc/mixin/impl/MixinRenderPlayerJBRA.java
+++ src/main/java/kamkeel/npcdbc/mixin/impl/MixinRenderPlayerJBRA.java
@@ -1,6 +1,7 @@
 package kamkeel.npcdbc.mixin.impl;
 
 import JinRyuu.JBRA.RenderPlayerJBRA;
+import kamkeel.npcdbc.skills.Transform;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,11 +9,12 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(value = RenderPlayerJBRA.class, remap = false)
 public abstract class MixinRenderPlayerJBRA extends RenderPlayer {
-   // @ModifyVariable(method = "renderEquippedItemsJBRA", at = @At(value = "STORE"), ordinal = 1, remap = false)
-
     // @ModifyVariable(method = "renderEquippedItemsJBRA", at = @At(value = "STORE"), ordinal = 1, remap = false)
+
+    @ModifyVariable(method = "renderEquippedItemsJBRA", at = @At(value = "STORE"), name = "rg", remap = false)
     public int modifyRage(int original) {
         System.out.println("hiiii " + original);
+        original = Transform.rage > 0 ? (int) Transform.rage : original;
 
         return original;
     }
