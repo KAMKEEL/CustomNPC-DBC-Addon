70f98d9d718fb6d0b49317bb4415b5b89063f439
Kam <KamronGaming@gmail.com>
2024-04-15 02:02:06 -0400

    Event Example

--- src/main/java/kamkeel/npcdbc/events/DBCEventHooks.java
+++ src/main/java/kamkeel/npcdbc/events/DBCEventHooks.java
@@ -1,19 +1,27 @@
 package kamkeel.npcdbc.events;
 
 import kamkeel.npcdbc.constants.DBCScriptType;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.entity.player.EntityPlayer;
 import noppes.npcs.api.entity.IPlayer;
-import noppes.npcs.constants.EnumScriptType;
+import noppes.npcs.controllers.ScriptController;
 import noppes.npcs.controllers.data.PlayerDataScript;
 import noppes.npcs.scripted.NpcAPI;
-import noppes.npcs.scripted.event.PlayerEvent;
+import noppes.npcs.scripted.event.DialogEvent;
 
 public class DBCEventHooks {
 
-    public static void onFormChangeEvent(PlayerDataScript handler, IPlayer player, int formBefore, int formAfter) {
-        DBCPlayerEvent.FormChangeEvent event = new DBCPlayerEvent.FormChangeEvent(player, 0, 0);
-        handler.callScript(DBCScriptType.FORMCHANGE.function, event);
-        NpcAPI.EVENT_BUS.post(event);
+    public static boolean onFormChangeEvent(DBCPlayerEvent.FormChangeEvent formChangeEvent) {
+        PlayerDataScript handler = ScriptController.Instance.playerScripts;
+        handler.callScript(DBCScriptType.FORMCHANGE.function, formChangeEvent);
+        return NpcAPI.EVENT_BUS.post(formChangeEvent);
     }
+
+    // HOW TO CANCEL AN EVENT. Must be @Cancelable
+    // This will post it as well as call the script.
+//    public static void something(){
+//        DBCPlayerEvent.FormChangeEvent formChangeEvent = new DBCPlayerEvent.FormChangeEvent((IPlayer)NpcAPI.Instance().getIEntity((EntityPlayer)player), 0, 0);
+//        if (DBCEventHooks.onFormChangeEvent(formChangeEvent)) {
+//            return;
+//        }
+//    }
 }
