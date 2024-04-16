e59b58d552a8a00faeb1ec2f9d4793b446dffc91
Kam <KamronGaming@gmail.com>
2024-04-16 03:55:37 -0400

    Merge Server Events

--- src/main/java/kamkeel/npcdbc/events/ServerEvents.java
+++ src/main/java/kamkeel/npcdbc/events/ServerEvents.java
@@ -26,21 +26,14 @@ public class ServerEvents {
             PerfectSync.registerAllDatas(event.entity); // entity registry
     }
 
-    /**
-     * loads/registers all PerfectSync datas from server NBTs
-     */
-    @SideOnly(Side.SERVER)
+
     @SubscribeEvent
-    public void worldTick(TickEvent.WorldTickEvent event) {
-        if (!Utility.isServer())
-            return;
-        if (!event.phase.equals(TickEvent.Phase.END))
+    public void onServerTick(TickEvent.PlayerTickEvent event) {
+        if (event.player == null || event.player.worldObj == null)
             return;
 
-        World w = event.world;
-        for (Object en : w.loadedEntityList) {
-            Entity player = (Entity) en;
-
+        EntityPlayer player = event.player;
+        if (event.side == Side.SERVER && event.phase == TickEvent.Phase.START) {
             if (player instanceof EntityPlayer) {// checks if player is eligible for data or not, incase they weren't before
                 PerfectSync.registerAllDatas(player);
                 if (DBCData.has(player))
@@ -52,17 +45,8 @@ public class ServerEvents {
             }
             if (player.ticksExisted % PerfectSync.SaveEveryXTicks == 0) //saves once every SaveEveryXTicks
                 PerfectSync.saveAllDatas(player, true);
-        }
-    }
 
-    @SideOnly(Side.SERVER)
-    @SubscribeEvent
-    public void onServerTick(TickEvent.PlayerTickEvent event) {
-        if (event.player == null || event.player.worldObj == null)
-            return;
 
-        if (event.side == Side.SERVER && event.phase == TickEvent.Phase.START) {
-            EntityPlayer player = event.player;
             if (PlayerDataController.Instance != null) {
                 PlayerData playerData = PlayerDataController.Instance.getPlayerData(player);
                 if(((IPlayerFormData)  playerData).getFormUpdate()) {
