74645956e05dbcabc9dc06acdf6e211543165888
Kam <KamronGaming@gmail.com>
2024-04-16 03:55:29 -0400

    JRM Core Fix

--- src/main/java/kamkeel/npcdbc/mixin/impl/MixinJRMCoreH.java
+++ src/main/java/kamkeel/npcdbc/mixin/impl/MixinJRMCoreH.java
@@ -20,64 +20,58 @@ public class MixinJRMCoreH {
     private static void go1(EntityPlayer player, int[] currAttributes, int attribute, int st, int st2, int race, String SklX, int currRelease, int arcRel, boolean legendOn, boolean majinOn, boolean kaiokenOn, boolean mysticOn, boolean uiOn, boolean GoDOn, int powerType, String[] Skls, boolean isFused, String majinAbs, CallbackInfoReturnable<Integer> info) {
         {
             PlayerCustomFormData formData = Utility.isServer() ? Utility.getFormData(player) : Utility.getFormDataClient();
-            System.out.println("Hi");
             if (formData != null && formData.isInCustomForm()) {
-                int skillX = powerType == 1 ? JRMCoreH.SklLvlX(1, SklX) - 1 : 0;
-                int mysticLvl = powerType == 1 ? JRMCoreH.SklLvl(10, 1, Skls) : 0;
-                int result = 0;
-                mysticOn = false;
-                uiOn = false;
-                GoDOn = false;
-                switch (race) {
-                    case 0:
-                        result = JRMCoreH.getAttributeHuman(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
-                        break;
-                    case 1:
-                        result = JRMCoreH.getAttributeSaiyan(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
-                        break;
-                    case 2:
-                        result = JRMCoreH.getAttributeHalfSaiyan(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
-                        break;
-                    case 3:
-                        result = JRMCoreH.getAttributeNamekian(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
-                        break;
-                    case 4:
-                        result = JRMCoreH.getAttributeArcosian(player, currAttributes, attribute, st, currRelease, arcRel, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
-                        break;
-                    case 5:
-                        result = JRMCoreH.getAttributeMajin(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn, majinAbs);
-                        break;
-                    default:
-                        result = currAttributes[attribute];
-                }
-                System.out.println("Hi2");
-
-                DBCData d = DBCData.get(player);
-                System.out.println("Hi52");
                 CustomForm f = formData.getCurrentForm();
-                System.out.println("Hi53");
-                float[] multis = formData.getCurrentForm().getAllMulti();
-                System.out.println("Hi54");
-                float stackableMulti = d.isForm(DBCForm.Kaioken) ? f.getFormMulti(DBCForm.Kaioken) : d.isForm(DBCForm.UltraInstinct) ? f.getFormMulti(DBCForm.UltraInstinct) : d.isForm(DBCForm.GodOfDestruction) ? f.getFormMulti(DBCForm.GodOfDestruction) : d.isForm(DBCForm.Mystic) ? f.getFormMulti(DBCForm.Mystic) : 1.0f;
-                System.out.println("Hi5");
+                if(f != null){
+                    int skillX = powerType == 1 ? JRMCoreH.SklLvlX(1, SklX) - 1 : 0;
+                    int mysticLvl = powerType == 1 ? JRMCoreH.SklLvl(10, 1, Skls) : 0;
+                    int result = 0;
+                    mysticOn = false;
+                    uiOn = false;
+                    GoDOn = false;
+                    switch (race) {
+                        case 0:
+                            result = JRMCoreH.getAttributeHuman(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
+                            break;
+                        case 1:
+                            result = JRMCoreH.getAttributeSaiyan(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
+                            break;
+                        case 2:
+                            result = JRMCoreH.getAttributeHalfSaiyan(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
+                            break;
+                        case 3:
+                            result = JRMCoreH.getAttributeNamekian(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
+                            break;
+                        case 4:
+                            result = JRMCoreH.getAttributeArcosian(player, currAttributes, attribute, st, currRelease, arcRel, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn);
+                            break;
+                        case 5:
+                            result = JRMCoreH.getAttributeMajin(player, currAttributes, attribute, st, skillX, mysticOn, mysticLvl, isFused, uiOn, powerType, GoDOn, majinAbs);
+                            break;
+                        default:
+                            result = currAttributes[attribute];
+                    }
+                    DBCData d = DBCData.get(player);
+                    float[] multis = formData.getCurrentForm().getAllMulti();
+                    float stackableMulti = d.isForm(DBCForm.Kaioken) ? f.getFormMulti(DBCForm.Kaioken) : d.isForm(DBCForm.UltraInstinct) ? f.getFormMulti(DBCForm.UltraInstinct) : d.isForm(DBCForm.GodOfDestruction) ? f.getFormMulti(DBCForm.GodOfDestruction) : d.isForm(DBCForm.Mystic) ? f.getFormMulti(DBCForm.Mystic) : 1.0f;
 
-                if (d.isForm(DBCForm.Kaioken) && d.State2 > 1)
-                    stackableMulti += stackableMulti * f.getState2Factor(DBCForm.Kaioken) * d.State2 / (JRMCoreH.TransKaiDmg.length - 1);
-                if (d.isForm(DBCForm.UltraInstinct) && d.State2 > 1)
-                    stackableMulti += stackableMulti * f.getState2Factor(DBCForm.UltraInstinct) * d.State2 / JGConfigUltraInstinct.CONFIG_UI_LEVELS;
-                System.out.println("Hi34");
+                    if (d.isForm(DBCForm.Kaioken) && d.State2 > 1)
+                        stackableMulti += stackableMulti * f.getState2Factor(DBCForm.Kaioken) * d.State2 / (JRMCoreH.TransKaiDmg.length - 1);
+                    if (d.isForm(DBCForm.UltraInstinct) && d.State2 > 1)
+                        stackableMulti += stackableMulti * f.getState2Factor(DBCForm.UltraInstinct) * d.State2 / JGConfigUltraInstinct.CONFIG_UI_LEVELS;
 
-                if (attribute == 0) //str
-                    result *= multis[0];
-                if (attribute == 1) //dex
-                    result *= multis[1];
-                if (attribute == 3) //will
-                    result *= multis[2];
-                System.out.println("hi3");
-                result *= stackableMulti;
-                result = (int) ((double) result > Double.MAX_VALUE ? Double.MAX_VALUE : (double) result);
+                    if (attribute == 0) //str
+                        result *= multis[0];
+                    if (attribute == 1) //dex
+                        result *= multis[1];
+                    if (attribute == 3) //will
+                        result *= multis[2];
+                    System.out.println("END END");
+                    result *= stackableMulti;
+                    result = (int) ((double) result > Double.MAX_VALUE ? Double.MAX_VALUE : (double) result);
 
-                info.setReturnValue(result);
+                    info.setReturnValue(result);
+                }
             }
         }
     }
