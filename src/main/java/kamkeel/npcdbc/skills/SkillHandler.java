e6c7e42a6880755b698e9cec61ec2057890c5b50
Kam <KamronGaming@gmail.com>
2024-04-17 03:11:29 -0400

    Fix Key Binds

--- src/main/java/kamkeel/npcdbc/skills/SkillHandler.java
+++ src/main/java/kamkeel/npcdbc/skills/SkillHandler.java
@@ -1,43 +1,36 @@
 package kamkeel.npcdbc.skills;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.InputEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
-import kamkeel.addon.client.DBCClient;
+import cpw.mods.fml.relauncher.Side;
 import kamkeel.npcdbc.client.KeyHandler;
 import kamkeel.npcdbc.util.Utility;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
-import noppes.npcs.NoppesUtilPlayer;
-import noppes.npcs.client.ClientProxy;
-import noppes.npcs.client.NoppesUtil;
-import noppes.npcs.client.gui.player.inventory.*;
-import noppes.npcs.constants.EnumPlayerPacket;
-import org.lwjgl.input.Keyboard;
 
 
 public class SkillHandler {
-    private static Minecraft mc = Minecraft.getMinecraft();
     @SubscribeEvent
-    public void onKey(InputEvent.KeyInputEvent event){
-        if(KeyHandler.AscendKey.isPressed()){
+    public void onSkill(TickEvent.PlayerTickEvent event){
+        if(event.side == Side.SERVER || event.player == null)
+            return;
+
+        if(event.phase == TickEvent.Phase.START){
             Minecraft mc = Minecraft.getMinecraft();
-            if(mc.currentScreen == null){
-                AscendKey();
+            if(mc.currentScreen == null && KeyHandler.AscendKey.getIsKeyPressed()){
+                performAscend();
+            }
+            else {
+                Transform.decrementRage();
             }
         }
     }
 
-    private void AscendKey() {
-        if (Keyboard.isKeyDown(KeyHandler.AscendKey.getKeyCode())) {
-            if (Utility.getFormDataClient().getSelectedForm() != null) {
-                Transform.Ascend(Utility.getFormDataClient().getSelectedForm());
-            } else {
-                Utility.sendMessage(mc.thePlayer, "§cYou have not selected a custom form!");
-                //make this a single time text
-            }
-        } else
-            Transform.decrementRage();
+    private void performAscend() {
+        if (Utility.getFormDataClient().getSelectedForm() != null) {
+            Transform.Ascend(Utility.getFormDataClient().getSelectedForm());
+        } else {
+            Minecraft mc = Minecraft.getMinecraft();
+            Utility.sendMessage(mc.thePlayer, "§cYou have not selected a custom form!");
+        }
     }
 }
