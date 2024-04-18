e26533c363922d2283e05dc85097ee4b9fdfca1b
bigguy345 <monkeybar3200@gmail.com>
2024-04-18 08:27:18 +0200

    Injected into correct method

--- src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinJRMCoreHDBC.java
+++ src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinJRMCoreHDBC.java
@@ -10,9 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = JRMCoreHDBC.class, remap = false)
 public class MixinJRMCoreHDBC {
-    @Inject(method = "getPlayerColor2", at = @At("HEAD"), cancellable = true)
-    private static void onGetPlayerColor2(int t, int d, int p, int r, int s, boolean v, boolean y, boolean ui, boolean gd, CallbackInfoReturnable<Integer> ci) {
-        if (!Utility.stackTraceContains("RenderPlayerJBRA")) { //if this method is not called by RenderPlayerJBRA, as rendering data is already handled, and this method is heavily used in rendering in DBC
+    @Inject(method = "getPlayerColor", at = @At("HEAD"), cancellable = true)
+    private static void onGetPlayerColor(int type, int def, int p, int r, int s, boolean divine, boolean y, boolean ui, boolean ui2, boolean gd, CallbackInfoReturnable<Integer> ci) {
+        //if this method is not called by RenderPlayerJBRA, as rendering data is already handled, and this method is heavily used in rendering in DBC
+        if (!Utility.stackTraceContains("RenderPlayerJBRA")) {
             PlayerCustomFormData formData = Utility.getFormDataClient();
             if (formData.isInCustomForm())
                 ci.setReturnValue(formData.getCurrentForm().hairColor);
