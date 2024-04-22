47ab1d45cd7bce502c91dc8876f3a2e58c522f0c
Kam <KamronGaming@gmail.com>
2024-04-22 00:47:39 -0400

    Update Form.java

--- src/main/java/kamkeel/npcdbc/data/form/Form.java
+++ src/main/java/kamkeel/npcdbc/data/form/Form.java
@@ -21,8 +21,8 @@ public class Form implements IForm {
 
     public int id = -1; // Only for internal usage
     public String name = "";
-    //name to be displayed in DBC GUI after "Form: ", preferably short as space is narrow
-    public String menuName = "§2§lCustom Form";
+
+    public String menuName = "§aNEW";
     public int race = DBCRace.ALL;
 
     public FormMastery mastery = new FormMastery(this);
