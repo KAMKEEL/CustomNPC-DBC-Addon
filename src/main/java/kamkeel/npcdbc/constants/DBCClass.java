e3468ad0c1d55b5b1743df22d4dfbbcb4974a323
Kam <KamronGaming@gmail.com>
2024-04-24 14:11:38 -0400

    DBC Class constants

---
+++ src/main/java/kamkeel/npcdbc/constants/DBCClass.java
@@ -0,0 +1,7 @@
+package kamkeel.npcdbc.constants;
+
+public class DBCClass {
+    public static final int MartialArtist = 0;
+    public static final int Spiritualist = 1;
+    public static final int Warrior = 2;
+}
