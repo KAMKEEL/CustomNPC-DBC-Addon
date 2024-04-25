5eb45440948fd380167358297c8a012d50759cb1
Kam <KamronGaming@gmail.com>
2024-04-25 18:45:42 -0400

    Update Java Doc BETA 2

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"kamkeel.npcdbc.api"},{"l":"kamkeel.npcdbc.api.event"},{"l":"kamkeel.npcdbc.api.form"},{"l":"kamkeel.npcdbc.api.npc"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"kamkeel.npcdbc.api"},{"l":"kamkeel.npcdbc.api.aura"},{"l":"kamkeel.npcdbc.api.event"},{"l":"kamkeel.npcdbc.api.form"},{"l":"kamkeel.npcdbc.api.npc"}]
