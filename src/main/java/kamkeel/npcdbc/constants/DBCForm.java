6b5b93ce4869b405a9ad2f578e110a12508435cb
bigguy345 <monkeybar3200@gmail.com>
2024-04-27 06:16:00 +0300

    Added all form constants to DBCForm

--- src/main/java/kamkeel/npcdbc/constants/DBCForm.java
+++ src/main/java/kamkeel/npcdbc/constants/DBCForm.java
@@ -1,6 +1,16 @@
 package kamkeel.npcdbc.constants;
 
 public class DBCForm {
+    public static final int Base = 0;
+
+    ////////////////////////////////
+    ////////////////////////////////
+    // Human forms
+    public static final int HumanBuffed = 1;
+    public static final int HumanFullRelease = 2;
+    public static final int HumanGod = 3;
+
+
     ////////////////////////////////
     ////////////////////////////////
     // Saiyan forms
@@ -16,6 +26,34 @@ public class DBCForm {
     public static final int SaiyanGod = 9;
     public static final int SaiyanBlue = 10;
     public static final int Shinka = 15; // SSJ Blue Evolved
+
+
+    ////////////////////////////////
+    ////////////////////////////////
+    // Namekian forms
+    public static final int NamekGiant = 1;
+    public static final int NamekFullRelease = 2;
+    public static final int NamekGod = 3;
+
+    ////////////////////////////////
+    ////////////////////////////////
+    // Arco forms
+    public static final int Minimal = 0;
+    public static final int FirstForm = 1;
+    public static final int SecondForm = 2;
+    public static final int ThirdForm = 3;
+    public static final int FinalForm = 4;
+    public static final int SuperForm = 5; //Ultimate Cooler
+    public static final int UltimateForm = 6; //Golden Frieza
+    public static final int ArcoGod = 7;
+
+    ////////////////////////////////
+    ////////////////////////////////
+    // Majin forms
+    public static final int MajinEvil = 1;
+    public static final int MajinFullPower = 2;
+    public static final int MajinPure = 3;
+    public static final int MajinGod = 4;
 
     ////////////////////////////////
     ////////////////////////////////
