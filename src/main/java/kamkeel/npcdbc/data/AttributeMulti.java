d328bf37578c1495fb3c00557e466847d758a1ed
bigguy345 <monkeybar3200@gmail.com>
2024-04-27 08:02:50 +0300

    Added AttributeMulti

---
+++ src/main/java/kamkeel/npcdbc/data/AttributeMulti.java
@@ -0,0 +1,8 @@
+package kamkeel.npcdbc.data;
+
+import kamkeel.npcdbc.api.IAttributeMulti;
+
+public class AttributeMulti implements IAttributeMulti {
+    public String name = "", operationType = "*";
+
+}
