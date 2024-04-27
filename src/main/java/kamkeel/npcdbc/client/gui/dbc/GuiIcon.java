96cee32a8efcf8e14024b8005d3e97712c64b138
somehussar <148253652+somehussar@users.noreply.github.com>
2024-04-28 00:30:26 +0200

    Icon Rendering

---
+++ src/main/java/kamkeel/npcdbc/client/gui/dbc/GuiIcon.java
@@ -0,0 +1,44 @@
+package kamkeel.npcdbc.client.gui.dbc;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiLabel;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+public class GuiIcon extends GuiLabel {
+    private final ResourceLocation texture;
+    private final int xPosition;
+    private final int yPosition;
+    private final int textureX;
+    private final int textureY;
+    private final int width;
+    private final int height;
+
+    public GuiIcon(ResourceLocation texture, int xPosition, int yPosition, int textureX, int textureY, int width, int height){
+
+        this.texture = texture;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.textureX = textureX;
+        this.textureY = textureY;
+        this.width = width;
+        this.height = height;
+    }
+
+
+    /**
+     * The draw function for the button
+     * @param client
+     * @param mouseX
+     * @param mouseY
+     */
+    @Override
+    public void func_146159_a(Minecraft client, int mouseX, int mouseY){
+        GL11.glPushMatrix();
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        client.getTextureManager().bindTexture(texture);
+        this.drawTexturedModalRect(xPosition, yPosition, textureX, textureY, width, height);
+        GL11.glPopMatrix();
+    }
+
+}
