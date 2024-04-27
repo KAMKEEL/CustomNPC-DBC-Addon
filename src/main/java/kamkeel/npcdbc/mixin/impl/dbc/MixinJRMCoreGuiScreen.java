485cd37c510ed8bdac52586444c3d3a043623fc6
somehussar <148253652+somehussar@users.noreply.github.com>
2024-04-27 04:02:44 +0200

    Add override only if player's powertype is 1 (DBC)

--- src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinJRMCoreGuiScreen.java
+++ src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinJRMCoreGuiScreen.java
@@ -3,6 +3,7 @@ package kamkeel.npcdbc.mixin.impl.dbc;
 import JinRyuu.JRMCore.JRMCoreGuiScreen;
 import cpw.mods.fml.common.FMLCommonHandler;
 import kamkeel.npcdbc.client.gui.dbc.StatSheetGUI;
+import kamkeel.npcdbc.data.DBCData;
 import kamkeel.npcdbc.mixin.IDBCGuiScreen;
 import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -32,7 +33,7 @@ public class MixinJRMCoreGuiScreen extends GuiScreen implements IDBCGuiScreen {
 
     @Inject(method = "updateScreen", at=@At("HEAD"), remap = true)
     private void onUpdateScreen(CallbackInfo ci){
-        if(this.guiID == 10)
+        if(this.guiID == 10 && StatSheetGUI.overrideBaseDBC && DBCData.getClient().Powertype == 1)
             FMLCommonHandler.instance().showGuiScreen(new StatSheetGUI());
     }
 
