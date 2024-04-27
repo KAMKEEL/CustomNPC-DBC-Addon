a7ca63da668cb2c820c921c7b3e5cdbb45a1a5fb
somehussar <148253652+somehussar@users.noreply.github.com>
2024-04-27 04:50:13 +0200

    Constructor argument to act as the replacement ID

--- src/main/java/kamkeel/npcdbc/client/gui/dbc/AbstractJRMCGui.java
+++ src/main/java/kamkeel/npcdbc/client/gui/dbc/AbstractJRMCGui.java
@@ -7,14 +7,26 @@ import org.lwjgl.opengl.GL11;
 
 public abstract class AbstractJRMCGui extends GuiScreen {
     private static final ResourceLocation menuTexture = new ResourceLocation("jinryuumodscore:gui.png");
-    private int menuImageWidth = 256;
-    private int menuImageHeight = 159;
+    private final int guiID;
+    protected int menuImageWidth = 256;
+    protected int menuImageHeight = 159;
+
+    /**
+     * @param guiReplacementID ID of the JRMC Gui this object is replacing
+     */
+    public AbstractJRMCGui(int guiReplacementID){
+        this.guiID = guiReplacementID;
+    }
 
     protected void drawBackground(){
         GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         this.mc.getTextureManager().bindTexture(menuTexture);
         this.drawTexturedModalRect((this.width-menuImageWidth)/2, (this.height-menuImageHeight)/2,0, 0, menuImageWidth, menuImageHeight);
+    }
 
+    @Override
+    public void initGui(){
+        addNavbarButtons();
     }
 
     @Override
@@ -25,5 +37,9 @@ public abstract class AbstractJRMCGui extends GuiScreen {
     @Override
     protected void actionPerformed(GuiButton button){
 
-    };
+    }
+
+    protected void addNavbarButtons(){
+        //@TODO Add proper navbar generation buttons
+    }
 }
