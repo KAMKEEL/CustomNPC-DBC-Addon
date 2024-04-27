caa1c74aa3fa03bca8b7b1606f09ca91d89fe298
somehussar <148253652+somehussar@users.noreply.github.com>
2024-04-27 06:37:47 +0200

    Render status effects

--- src/main/java/kamkeel/npcdbc/client/gui/dbc/StatSheetGui.java
+++ src/main/java/kamkeel/npcdbc/client/gui/dbc/StatSheetGui.java
@@ -24,6 +24,7 @@ public class StatSheetGui extends AbstractJRMCGui {
     public void drawScreen(int mouseX, int mouseY, float partialTicks){
         this.drawBackground();
 
+        addStatusEffects();
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
@@ -31,27 +32,7 @@ public class StatSheetGui extends AbstractJRMCGui {
     public void initGui(){
         super.initGui();
 
-//        this.buttonList.add(new GuiButton(31, 0, 0, "News"));
-//
-//        //Only render these if powertype == 1
-//        //BEGIN
-//        this.buttonList.add(new GuiButton(11, 0, 20, "Skills"));
-//        this.buttonList.add(new GuiButton(12, 0, 40, "ki"));
-//        this.buttonList.add(new GuiButton(17, 0, 60, "Training"));
-//        this.buttonList.add(new GuiButton(60, 0, 80, "Story"));
-//        this.buttonList.add(new GuiButton(70, 0, 100, "Group Management"));
-//        //END
-//
-//
-//        this.buttonList.add(new GuiButton(40, 0, 140, "ServerConfig"));
-//
-//
-//        this.buttonList.add(new GuiButton(10000, 0, 180, "ClientSettings"));
-//        this.buttonList.add(new GuiButton(10001, 0, 200, "Help"));
-//        this.buttonList.add(new GuiButton(10011, 0, 220, "Notifications"));
-//
-//        this.buttonList.add(new GuiButton(14, 0, 260, "Difficulty"));
-//        this.buttonList.add(new GuiButton(80, 0, 280, "Server Shop"));
+        addServerButtons();
 
 
 
