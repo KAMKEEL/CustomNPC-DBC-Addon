6c8ee03490af1a4892da2a41777a7d9e29d0aed7
somehussar <148253652+somehussar@users.noreply.github.com>
2024-04-27 06:25:33 +0200

    Navigation bar rendering

--- src/main/java/kamkeel/npcdbc/client/gui/dbc/AbstractJRMCGui.java
+++ src/main/java/kamkeel/npcdbc/client/gui/dbc/AbstractJRMCGui.java
@@ -1,5 +1,10 @@
 package kamkeel.npcdbc.client.gui.dbc;
 
+import JinRyuu.JRMCore.JRMCoreGuiButtons00;
+import JinRyuu.JRMCore.JRMCoreGuiButtons02;
+import JinRyuu.JRMCore.JRMCoreGuiButtons03;
+import JinRyuu.JRMCore.JRMCoreH;
+import kamkeel.npcdbc.client.gui.dbc.constants.GuiButtonConstants;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.ResourceLocation;
@@ -10,6 +15,9 @@ public abstract class AbstractJRMCGui extends GuiScreen {
     private final int guiID;
     protected int menuImageWidth = 256;
     protected int menuImageHeight = 159;
+
+    protected int guiWidthOffset;
+    protected int guiHeightOffset;
 
     /**
      * @param guiReplacementID ID of the JRMC Gui this object is replacing
@@ -26,6 +34,9 @@ public abstract class AbstractJRMCGui extends GuiScreen {
 
     @Override
     public void initGui(){
+        this.guiWidthOffset = (this.width - menuImageWidth) / 2;
+        this.guiHeightOffset = (this.height - menuImageHeight) / 2;
+
         addNavbarButtons();
     }
 
@@ -36,10 +47,46 @@ public abstract class AbstractJRMCGui extends GuiScreen {
 
     @Override
     protected void actionPerformed(GuiButton button){
-
+        switch(button.id){
+            case 0:
+                this.mc.thePlayer.closeScreen();
+                return;
+            default:
+                break;
+        }
     }
 
     protected void addNavbarButtons(){
         //@TODO Add proper navbar generation buttons
+        this.guiWidthOffset = (this.width - menuImageWidth) / 2;
+        this.guiHeightOffset = (this.height - menuImageHeight) / 2;
+
+        this.buttonList.add(new JRMCoreGuiButtons00(GuiButtonConstants.EXIT, guiWidthOffset + menuImageWidth/2 -150, guiHeightOffset+menuImageHeight/2+65, 20, 20, "X", 0));
+
+        GuiButtonConstants.ReferenceIDs[] guiReferences = GuiButtonConstants.ReferenceIDs.values();
+        int i = 0;
+        for(; i < guiReferences.length; i++){
+            GuiButtonConstants.ReferenceIDs ref = guiReferences[i];
+            if(ref == GuiButtonConstants.ReferenceIDs.DIFFICULTY)
+                break;
+
+            boolean isSelected = ref.getGuiID() == this.guiID;
+            this.buttonList.add(new JRMCoreGuiButtons03(ref.getGuiID(), guiWidthOffset + i*21, guiHeightOffset+menuImageHeight+2, "",  (isSelected ? 1 : 0), 8046079,  ref.getIconID()));
+            //@TODO ADD TOOLTIP
+        }
+
+        if(JRMCoreH.JYC()){
+            //@TODO get name
+            this.buttonList.add(new JRMCoreGuiButtons02(11, guiWidthOffset + i*21, guiHeightOffset+menuImageHeight+2, "CA", 0, 8046079));
+            //@TODO add tooltip
+            i++;
+        }
+        if(JRMCoreH.JFC()){
+            //@TODO get name
+            this.buttonList.add(new JRMCoreGuiButtons02(12, guiWidthOffset + i*21, guiHeightOffset+menuImageHeight+2, "FA", 0, 8046079));
+            //@TODO add tooltip
+            i++;
+        }
+
     }
 }
