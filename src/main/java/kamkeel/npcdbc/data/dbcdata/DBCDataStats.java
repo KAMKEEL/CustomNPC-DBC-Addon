e34d9e226d872fb887f2dad3aaa1766291e16b71
Kam <KamronGaming@gmail.com>
2024-04-28 01:51:48 -0400

    Final DBC Data

--- src/main/java/kamkeel/npcdbc/data/dbcdata/DBCDataStats.java
+++ src/main/java/kamkeel/npcdbc/data/dbcdata/DBCDataStats.java
@@ -25,19 +25,19 @@ import static JinRyuu.JRMCore.JRMCoreH.getMajinAbsorptionValueS;
 import static JinRyuu.JRMCore.JRMCoreH.nbt;
 
 public class DBCDataStats {
-    DBCData d;
+    private final DBCData data;
 
     public DBCDataStats(DBCData dbcData) {
-        this.d = dbcData;
+        this.data = dbcData;
     }
 
     public HashMap<Integer, PlayerEffect> getPlayerEffects() {
-        return d.currentEffects;
+        return data.currentEffects;
     }
 
     public void setCurrentEffects(HashMap<Integer, PlayerEffect> setVals) {
-        NBTTagCompound raw = d.getRawCompound();
-        d.currentEffects = updateEffects(setVals);
+        NBTTagCompound raw = data.getRawCompound();
+        data.currentEffects = updateEffects(setVals);
         saveEffectsNBT(raw);
     }
 
@@ -45,8 +45,8 @@ public class DBCDataStats {
         HashMap<Integer, PlayerEffect> createdMap = new HashMap<>();
         for (PlayerEffect playerEffect : setVals.values()) {
             PlayerEffect newEffect;
-            if (d.currentEffects.containsKey(playerEffect.id)) {
-                newEffect = d.currentEffects.get(playerEffect.id);
+            if (data.currentEffects.containsKey(playerEffect.id)) {
+                newEffect = data.currentEffects.get(playerEffect.id);
                 newEffect.duration = playerEffect.duration;
                 newEffect.level = playerEffect.level;
                 createdMap.put(playerEffect.id, newEffect);
@@ -59,7 +59,7 @@ public class DBCDataStats {
 
     public void saveEffectsNBT(NBTTagCompound nbt) {
         NBTTagList nbttaglist = new NBTTagList();
-        Iterator iterator = d.currentEffects.values().iterator();
+        Iterator iterator = data.currentEffects.values().iterator();
         while (iterator.hasNext()) {
             PlayerEffect playerEffect = (PlayerEffect) iterator.next();
             nbttaglist.appendTag(playerEffect.writeEffectData(new NBTTagCompound()));
@@ -68,7 +68,7 @@ public class DBCDataStats {
     }
 
     public void decrementActiveEffects() {
-        HashMap<Integer, PlayerEffect> currentEffects = StatusEffectController.Instance.playerEffects.get(Utility.getUUID(d.player));
+        HashMap<Integer, PlayerEffect> currentEffects = StatusEffectController.Instance.playerEffects.get(Utility.getUUID(data.player));
         Iterator<Map.Entry<Integer, PlayerEffect>> iterator = currentEffects.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<Integer, PlayerEffect> entry = iterator.next();
@@ -83,7 +83,7 @@ public class DBCDataStats {
             else if (currentEffect.duration <= 0) {
                 StatusEffect parent = StatusEffectController.Instance.get(currentEffect.id);
                 if (parent != null)
-                    parent.runout(d.player, currentEffect);
+                    parent.runout(data.player, currentEffect);
                 iterator.remove();
             } else
                 currentEffect.duration--;
@@ -92,33 +92,33 @@ public class DBCDataStats {
     }
 
     public int[] getAllAttributes() {
-        return new int[]{d.STR, d.DEX, d.CON, d.WIL, d.MND, d.SPI};
+        return new int[]{data.STR, data.DEX, data.CON, data.WIL, data.MND, data.SPI};
     }
 
     public int getFullAttribute(int attri) {
-        boolean majin = JRMCoreH.StusEfcts(12, d.StatusEffects);
-        boolean fusion = (JRMCoreH.StusEfcts(10, d.StatusEffects) || JRMCoreH.StusEfcts(11, d.StatusEffects));
-        boolean legendary = JRMCoreH.StusEfcts(14, d.StatusEffects);
-        boolean kaioken = JRMCoreH.StusEfcts(5, d.StatusEffects);
-        boolean mystic = JRMCoreH.StusEfcts(13, d.StatusEffects);
-        boolean ui = JRMCoreH.StusEfcts(19, d.StatusEffects);
-        boolean GoD = JRMCoreH.StusEfcts(20, d.StatusEffects);
+        boolean majin = JRMCoreH.StusEfcts(12, data.StatusEffects);
+        boolean fusion = (JRMCoreH.StusEfcts(10, data.StatusEffects) || JRMCoreH.StusEfcts(11, data.StatusEffects));
+        boolean legendary = JRMCoreH.StusEfcts(14, data.StatusEffects);
+        boolean kaioken = JRMCoreH.StusEfcts(5, data.StatusEffects);
+        boolean mystic = JRMCoreH.StusEfcts(13, data.StatusEffects);
+        boolean ui = JRMCoreH.StusEfcts(19, data.StatusEffects);
+        boolean GoD = JRMCoreH.StusEfcts(20, data.StatusEffects);
 
-        return JRMCoreH.getPlayerAttribute(d.player, getAllAttributes(), attri, d.State, d.State2, d.Race, d.RacialSkills, (int) d.Release, d.ArcReserve, legendary, majin, kaioken, mystic, ui, GoD, d.Powertype, d.Skills.split(","), fusion, d.MajinAbsorptionData);
+        return JRMCoreH.getPlayerAttribute(data.player, getAllAttributes(), attri, data.State, data.State2, data.Race, data.RacialSkills, (int) data.Release, data.ArcReserve, legendary, majin, kaioken, mystic, ui, GoD, data.Powertype, data.Skills.split(","), fusion, data.MajinAbsorptionData);
 
     }
 
     public int[] getAllFullAttributes() {
-        boolean majin = JRMCoreH.StusEfcts(12, d.StatusEffects);
-        boolean fusion = (JRMCoreH.StusEfcts(10, d.StatusEffects) || JRMCoreH.StusEfcts(11, d.StatusEffects));
-        boolean legendary = JRMCoreH.StusEfcts(14, d.StatusEffects);
-        boolean kaioken = JRMCoreH.StusEfcts(5, d.StatusEffects);
-        boolean mystic = JRMCoreH.StusEfcts(13, d.StatusEffects);
-        boolean ui = JRMCoreH.StusEfcts(19, d.StatusEffects);
-        boolean GoD = JRMCoreH.StusEfcts(20, d.StatusEffects);
+        boolean majin = JRMCoreH.StusEfcts(12, data.StatusEffects);
+        boolean fusion = (JRMCoreH.StusEfcts(10, data.StatusEffects) || JRMCoreH.StusEfcts(11, data.StatusEffects));
+        boolean legendary = JRMCoreH.StusEfcts(14, data.StatusEffects);
+        boolean kaioken = JRMCoreH.StusEfcts(5, data.StatusEffects);
+        boolean mystic = JRMCoreH.StusEfcts(13, data.StatusEffects);
+        boolean ui = JRMCoreH.StusEfcts(19, data.StatusEffects);
+        boolean GoD = JRMCoreH.StusEfcts(20, data.StatusEffects);
         int[] a = new int[6];
         for (int i = 0; i <= 5; i++)
-            a[i] = JRMCoreH.getPlayerAttribute(d.player, getAllAttributes(), i, d.State, d.State2, d.Race, d.RacialSkills, d.Release, d.ArcReserve, legendary, majin, kaioken, mystic, ui, GoD, d.Powertype, d.Skills.split(","), fusion, d.MajinAbsorptionData);
+            a[i] = JRMCoreH.getPlayerAttribute(data.player, getAllAttributes(), i, data.State, data.State2, data.Race, data.RacialSkills, data.Release, data.ArcReserve, legendary, majin, kaioken, mystic, ui, GoD, data.Powertype, data.Skills.split(","), fusion, data.MajinAbsorptionData);
 
         return a;
     }
@@ -128,12 +128,12 @@ public class DBCDataStats {
 
         if (att == 0) {
             int maxki = getMaxStat(5);
-            extraoutput = (int) (JRMCoreH.SklLvl(12, d.Skills.split(",")) * 0.0025 * maxki * release * 0.01);
+            extraoutput = (int) (JRMCoreH.SklLvl(12, data.Skills.split(",")) * 0.0025 * maxki * release * 0.01);
         } else if (att == 1) {
             int maxki = getMaxStat(5);
-            extraoutput = (int) (JRMCoreH.SklLvl(11, d.Skills.split(",")) * 0.005 * maxki * release * 0.01);
+            extraoutput = (int) (JRMCoreH.SklLvl(11, data.Skills.split(",")) * 0.005 * maxki * release * 0.01);
         } else if (att == 5)
-            extraoutput = getMaxStat(5) - JRMCoreH.stat(d.player, att, d.Powertype, att, d.SPI, d.Race, d.Class, JRMCoreH.SklLvl_KiBs(d.Skills.split(","), 1));
+            extraoutput = getMaxStat(5) - JRMCoreH.stat(data.player, att, data.Powertype, att, data.SPI, data.Race, data.Class, JRMCoreH.SklLvl_KiBs(data.Skills.split(","), 1));
 
         return extraoutput;
     }
@@ -146,8 +146,8 @@ public class DBCDataStats {
         else
             attribute = getAllAttributes()[attributeID];
 
-        float f = attributeID == 5 ? JRMCoreH.SklLvl_KiBs(d.Skills.split(","), 1) : 0f;
-        int stat = JRMCoreH.stat(d.player, attributeID, d.Powertype, attributeID, attribute, d.Race, d.Class, f);
+        float f = attributeID == 5 ? JRMCoreH.SklLvl_KiBs(data.Skills.split(","), 1) : 0f;
+        int stat = JRMCoreH.stat(data.player, attributeID, data.Powertype, attributeID, attribute, data.Race, data.Class, f);
 
         if (attributeID == 0)
             stat += getExtraOutput(attributeID, 100);
@@ -158,28 +158,28 @@ public class DBCDataStats {
     }
 
     public int getCurrentStat(int attribute) { // gets stat at current release
-        return (int) (getMaxStat(attribute) * d.Release * 0.01D * JRMCoreH.weightPerc(0, d.player));
+        return (int) (getMaxStat(attribute) * data.Release * 0.01D * JRMCoreH.weightPerc(0, data.player));
     }
 
     public double getCurrentMulti() {
-        return getFullAttribute(0) / d.STR;
+        return getFullAttribute(0) / data.STR;
 
     }
 
     public int getMaxBody() {
-        return JRMCoreH.stat(d.player, 2, d.Powertype, 2, d.CON, d.Race, d.Class, 0);
+        return JRMCoreH.stat(data.player, 2, data.Powertype, 2, data.CON, data.Race, data.Class, 0);
     }
 
     public int getMaxStamina() {
-        return JRMCoreH.stat(d.player, 2, d.Powertype, 3, d.CON, d.Race, d.Class, 0);
+        return JRMCoreH.stat(data.player, 2, data.Powertype, 3, data.CON, data.Race, data.Class, 0);
     }
 
     public int getMaxKi() {
-        return JRMCoreH.stat(d.player, 5, d.Powertype, 5, d.CON, d.Race, d.Class, 0);
+        return JRMCoreH.stat(data.player, 5, data.Powertype, 5, data.CON, data.Race, data.Class, 0);
     }
 
     public float getCurrentBodyPercentage() {
-        return (d.Body * 100) / (float) getMaxBody();
+        return (data.Body * 100) / (float) getMaxBody();
 
     }
 
@@ -188,16 +188,16 @@ public class DBCDataStats {
         int maxKi = getMaxKi();
         int toAdd = (int) (maxKi * (percToRestore / 100));
 
-        d.Ki = ValueUtil.clamp(d.Ki + toAdd, 0, maxKi);
-        d.getRawCompound().setInteger("jrmcEnrgy", d.Ki);
+        data.Ki = ValueUtil.clamp(data.Ki + toAdd, 0, maxKi);
+        data.getRawCompound().setInteger("jrmcEnrgy", data.Ki);
     }
 
     public void restoreHealthPercent(float percToRestore) {
         int maxBody = getMaxBody();
         int toAdd = (int) (maxBody * (percToRestore / 100));
 
-        d.Body = ValueUtil.clamp(d.Body + toAdd, 0, maxBody);
-        d.getRawCompound().setInteger("jrmcBdy", d.Body);
+        data.Body = ValueUtil.clamp(data.Body + toAdd, 0, maxBody);
+        data.getRawCompound().setInteger("jrmcBdy", data.Body);
     }
 
     public void restoreStaminaPercent(float percToRestore) {
@@ -205,61 +205,61 @@ public class DBCDataStats {
         int toAdd = (int) (maxSta * (percToRestore / 100));
 
 
-        d.Stamina = ValueUtil.clamp(d.Stamina + toAdd, 0, maxSta);
-        d.getRawCompound().setInteger("jrmcStamina", d.Stamina);
+        data.Stamina = ValueUtil.clamp(data.Stamina + toAdd, 0, maxSta);
+        data.getRawCompound().setInteger("jrmcStamina", data.Stamina);
     }
 
     //Negative value will add instead
     public void restoreUIHeat(float percToRestore) {
-        if (!d.isForm(DBCForm.UltraInstinct))
+        if (!data.isForm(DBCForm.UltraInstinct))
             return;
 
-        int maxHeat = JGConfigUltraInstinct.CONFIG_UI_HEAT_DURATION[d.State2];
+        int maxHeat = JGConfigUltraInstinct.CONFIG_UI_HEAT_DURATION[data.State2];
         int toAdd = (int) (maxHeat * (percToRestore / 100));
 
-        d.Heat = ValueUtil.clamp(d.Heat - toAdd, 0, maxHeat);
-        d.getRawCompound().setInteger("jrmcEf8slc", d.Heat);
+        data.Heat = ValueUtil.clamp(data.Heat - toAdd, 0, maxHeat);
+        data.getRawCompound().setInteger("jrmcEf8slc", data.Heat);
     }
 
     public void setArcReserve(int reserve) {
-        if (d.Race != 4)
+        if (data.Race != 4)
             return;
-        nbt(d.player).setInteger("jrmcArcRsrv", reserve);
+        nbt(data.player).setInteger("jrmcArcRsrv", reserve);
     }
 
     public void restoreArcPP(int percToRestoreFromMax) {
-        if (d.Race != 4)
+        if (data.Race != 4)
             return;
 
         int maxReserve = JRMCoreConfig.ArcosianPPMax[getMaxSkillX()];
         int toAdd = maxReserve * (percToRestoreFromMax / 100);
 
         // Arc Reserve can be less than 0, so Add from 0.
-        int reserve = Math.max(nbt(d.player).getInteger("jrmcArcRsrv"), 0);
+        int reserve = Math.max(nbt(data.player).getInteger("jrmcArcRsrv"), 0);
         reserve = ValueUtil.clamp(reserve + toAdd, 0, maxReserve);
         setArcReserve(reserve);
     }
 
     public void setAbsorption(int amount) {
-        if (d.Race != DBCRace.MAJIN)
+        if (data.Race != DBCRace.MAJIN)
             return;
 
-        nbt(d.player).setString("jrmcMajinAbsorptionData", amount + ",0,0+0");
+        nbt(data.player).setString("jrmcMajinAbsorptionData", amount + ",0,0+0");
     }
 
     public void restoreAbsorption(int percToRestoreFromMax) {
 
-        if (d.Race != DBCRace.MAJIN)
+        if (data.Race != DBCRace.MAJIN)
             return;
         int maxAbsorption = JGConfigRaces.CONFIG_MAJIN_ABSORPTON_MAX_LEVEL;
         int toAdd = (int) (maxAbsorption * (percToRestoreFromMax / 100f));
 
-        int currentAbsorption = getMajinAbsorptionValueS(nbt(d.player).getString("jrmcMajinAbsorptionData"));
+        int currentAbsorption = getMajinAbsorptionValueS(nbt(data.player).getString("jrmcMajinAbsorptionData"));
         setAbsorption(ValueUtil.clamp(toAdd + currentAbsorption, 0, maxAbsorption));
     }
 
     public int getMaxSkillX() {
-        String racial = nbt(d.player).getString("jrmcSSltX");
+        String racial = nbt(data.player).getString("jrmcSSltX");
         if (racial == null || racial.isEmpty() || racial.contains("pty"))
             return 0;
         return Integer.parseInt(racial.substring(2));
@@ -267,7 +267,7 @@ public class DBCDataStats {
 
     public int getJRMCPlayerID() {
         for (int pl = 0; pl < JRMCoreH.plyrs.length; pl++)
-            if (JRMCoreH.plyrs[pl].equals(d.player.getCommandSenderName()))
+            if (JRMCoreH.plyrs[pl].equals(data.player.getCommandSenderName()))
                 return pl;
         return 0;
     }
@@ -275,7 +275,7 @@ public class DBCDataStats {
 
     public String getJRMCData(int id) {
         for (int pl = 0; pl < JRMCoreH.plyrs.length; pl++) {
-            if (JRMCoreH.plyrs[pl].equals(d.player.getCommandSenderName())) {
+            if (JRMCoreH.plyrs[pl].equals(data.player.getCommandSenderName())) {
                 return JRMCoreH.data(id)[pl];
             }
         }
@@ -284,7 +284,7 @@ public class DBCDataStats {
     }
 
     public boolean isChargingKiAttack() {
-        ExtendedPlayer jrmcExtendedPlayer = ExtendedPlayer.get(d.player);
+        ExtendedPlayer jrmcExtendedPlayer = ExtendedPlayer.get(data.player);
 
         //Abusing JRMCore's animation system to see if a player is charging a ki attack.
         boolean kiAnimationTypeSelected = jrmcExtendedPlayer.getAnimKiShoot() != 0;
@@ -294,12 +294,12 @@ public class DBCDataStats {
     }
 
     public void applyNamekianRegen() {
-        if (d.player == null)
+        if (data.player == null)
             return;
 
         if (getCurrentBodyPercentage() < ConfigDBCGameplay.NamekianRegenMin) {
-            if (!StatusEffectController.getInstance().hasEffect(d.player, Effects.NAMEK_REGEN)) {
-                StatusEffectController.getInstance().applyEffect(d.player, new PlayerEffect(Effects.NAMEK_REGEN, -100, (byte) 1));
+            if (!StatusEffectController.getInstance().hasEffect(data.player, Effects.NAMEK_REGEN)) {
+                StatusEffectController.getInstance().applyEffect(data.player, new PlayerEffect(Effects.NAMEK_REGEN, -100, (byte) 1));
             }
         }
     }
