029e6b0cd94e89ed6cfbd553f2358053e749e254
bigguy345 <monkeybar3200@gmail.com>
2024-04-28 04:05:47 +0300

    Implemented Dodge and Damage Negation mastery functionality

--- src/main/java/kamkeel/npcdbc/ServerEventHandler.java
+++ src/main/java/kamkeel/npcdbc/ServerEventHandler.java
@@ -94,15 +94,18 @@ public class ServerEventHandler {
     public void DBCAttackEvent(DBCPlayerEvent.DamagedEvent event) {
         Form form = DBCData.getForm((EntityPlayer) event.player);
         if (form != null) {
+            PlayerDBCInfo formData = PlayerDataUtil.getDBCInfo((EntityPlayer) event.player);
             if (form.mastery.hasDodge()) {
                 Random rand = new Random();
-                if (form.mastery.dodgeChance <= rand.nextInt(100)) {
+                float dodgeChance = form.mastery.dodgeChance * form.mastery.calculateMulti("dodge", formData.getCurrentLevel());
+                if (dodgeChance >= rand.nextInt(100)) {
                     event.setCanceled(true);
                     Dodge.dodge((Entity) event.player, event.damageSource.getMCDamageSource().getEntity());
                 }
             }
             if (form.mastery.hasDamageNegation()) {
-                float newDamage = event.getDamage() * (100 - form.mastery.damageNegation) / 100;
+                float damageNegation = form.mastery.damageNegation * form.mastery.calculateMulti("damageNegation", formData.getCurrentLevel());
+                float newDamage = event.getDamage() * (100 - damageNegation) / 100;
                 event.setDamage(newDamage);
             }
 
