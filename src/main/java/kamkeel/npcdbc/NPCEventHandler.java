6bbd853ea5cea8ea4afc6391830311ece8ccde29
bigguy345 <monkeybar3200@gmail.com>
2024-04-29 02:39:01 +0300

    Fixed minor dodge bug

--- src/main/java/kamkeel/npcdbc/NPCEventHandler.java
+++ src/main/java/kamkeel/npcdbc/NPCEventHandler.java
@@ -1,32 +1,14 @@
 package kamkeel.npcdbc;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.relauncher.Side;
 import kamkeel.npcdbc.combatmode.Dodge;
-import kamkeel.npcdbc.config.ConfigDBCGameplay;
-import kamkeel.npcdbc.constants.DBCRace;
-import kamkeel.npcdbc.controllers.StatusEffectController;
 import kamkeel.npcdbc.data.PlayerDBCInfo;
 import kamkeel.npcdbc.data.dbcdata.DBCData;
 import kamkeel.npcdbc.data.form.Form;
-import kamkeel.npcdbc.mixin.IPlayerDBCInfo;
-import kamkeel.npcdbc.network.PacketHandler;
-import kamkeel.npcdbc.network.packets.CapsuleInfo;
-import kamkeel.npcdbc.network.packets.ChargingDexInfo;
 import kamkeel.npcdbc.scripted.DBCPlayerEvent;
 import kamkeel.npcdbc.util.PlayerDataUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.WorldServer;
-import net.minecraftforge.common.util.FakePlayer;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import noppes.npcs.NoppesUtilServer;
-import noppes.npcs.controllers.PlayerDataController;
-import noppes.npcs.controllers.data.PlayerData;
-import noppes.npcs.util.ValueUtil;
 
 import java.util.Random;
 
@@ -44,9 +26,10 @@ public class NPCEventHandler {
                 Random rand = new Random();
                 float dodgeChance = form.mastery.dodgeChance * form.mastery.calculateMulti("dodge", formData.getCurrentLevel());
                 if (dodgeChance >= rand.nextInt(100)) {
-                    event.setCanceled(true);
-                    Dodge.dodge((Entity) event.player, event.damageSource.getMCDamageSource().getEntity());
-                    return;
+                    if (Dodge.dodge((Entity) event.player, event.damageSource.getMCDamageSource().getEntity())) {
+                        event.setCanceled(true);
+                        return;
+                    }
                 }
             }
             if (form.mastery.hasDamageNegation()) {
