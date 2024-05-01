6b1f47e70d77496bb691a3de22f8b9fcedf0fbfd
somehussar <148253652+somehussar@users.noreply.github.com>
2024-05-01 02:01:12 +0200

    Move dark/lightmode switch to above the server button

--- src/main/java/kamkeel/npcdbc/client/gui/dbc/StatSheetGui.java
+++ src/main/java/kamkeel/npcdbc/client/gui/dbc/StatSheetGui.java
@@ -442,11 +442,11 @@ public class StatSheetGui extends AbstractJRMCGui {
         String s = "Switch to "+(ConfigDBCClient.EnhancedGui ? "Normal" : "§aEnhanced") +" GUI";
         int i = this.fontRendererObj.getStringWidth(s)+10;
         this.buttonList.add(new JRMCoreGuiButtons00(303030303, (this.width -i)/2, guiHeightOffset - 30, i + 8, 20, s, 0));
-        if(ConfigDBCClient.EnhancedGui){
-            String dark = ConfigDBCClient.DarkMode ? "Light" : "Dark";
-            int j = this.fontRendererObj.getStringWidth(dark)+10;
-            this.buttonList.add(new JRMCoreGuiButtons00(404040404, (this.width -j)/2 + 40, guiHeightOffset - 30, j + 8, 20, dark, 0));
-        }
+
+        String dark = ConfigDBCClient.DarkMode ? "Light" : "Dark";
+        int j = this.fontRendererObj.getStringWidth(dark)+10;
+        this.buttonList.add(new JRMCoreGuiButtons00(404040404, guiWidthOffset + 260, guiHeightOffset+5, j + 8, 20, dark, 0));
+
 
 
         //Difficulty button
