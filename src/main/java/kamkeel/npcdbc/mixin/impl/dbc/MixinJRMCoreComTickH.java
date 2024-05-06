64edb54d2b6e6547c0ecac98e5259340c0186a4d
Kam <KamronGaming@gmail.com>
2024-05-06 04:45:59 -0400

    Fix Potara Deletion

--- src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinJRMCoreComTickH.java
+++ src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinJRMCoreComTickH.java
@@ -1,14 +1,19 @@
 package kamkeel.npcdbc.mixin.impl.dbc;
 
 import JinRyuu.JRMCore.JRMCoreComTickH;
+import JinRyuu.JRMCore.i.ExtendedPlayer;
+import JinRyuu.JRMCore.i.InventoryCustomPlayer;
 import JinRyuu.JRMCore.server.JGPlayerMP;
 import kamkeel.npcdbc.CommonProxy;
+import kamkeel.npcdbc.items.Potara;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = JRMCoreComTickH.class, remap = false)
@@ -20,4 +25,10 @@ public class MixinJRMCoreComTickH {
         CommonProxy.CurrentJRMCTickPlayer = player;
     }
 
+    @Redirect(method = "updatePlayersData", at = @At(value = "INVOKE", target = "LJinRyuu/JRMCore/i/InventoryCustomPlayer;setInventorySlotContents(ILnet/minecraft/item/ItemStack;)V"))
+    public void preventPotaraDelete(InventoryCustomPlayer instance, int slot, ItemStack stack) {
+        if(slot > 2 && stack != null && !(stack.getItem() instanceof Potara)){
+            instance.setInventorySlotContents(slot, (ItemStack)null);
+        }
+    }
 }
