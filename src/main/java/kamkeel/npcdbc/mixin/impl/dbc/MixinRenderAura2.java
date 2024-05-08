c790e3c7673bca1ede92b64afcc2d325f7f3f5e9
bigguy345 <monkeybar3200@gmail.com>
2024-05-08 07:06:24 +0300

    Fixed npc aura size

--- src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinRenderAura2.java
+++ src/main/java/kamkeel/npcdbc/mixin/impl/dbc/MixinRenderAura2.java
@@ -9,7 +9,9 @@ import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import kamkeel.npcdbc.data.dbcdata.DBCData;
 import kamkeel.npcdbc.mixin.IEntityAura;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import noppes.npcs.entity.EntityCustomNpc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -49,13 +51,19 @@ public class MixinRenderAura2 {
     @Inject(method = "func_tad(LJinRyuu/DragonBC/common/Npcs/EntityAura2;DDDFF)V", at = @At(value = "INVOKE", target = "LJinRyuu/DragonBC/common/Npcs/EntityAura2;getState2()F", ordinal = 0, shift = At.Shift.BEFORE))
     private void fixAuraSize(EntityAura2 par1Entity, double parX, double parY, double parZ, float par8, float par9, CallbackInfo ci, @Local(name = "s1") LocalFloatRef s1, @Local(name = "s") LocalFloatRef s, @Local(name = "cr") LocalFloatRef cr) {
         EntityPlayer auraOwner = par1Entity.worldObj.getPlayerEntityByName(par1Entity.getmot());
-        if (auraOwner != null) {
+        if (auraOwner instanceof EntityPlayer) {
             DBCData dbcData = DBCData.get(auraOwner);
             if (dbcData.addonFormID > -1) {
                 float size = JRMCoreHDBC.DBCsizeBasedOnRace2(dbcData.Race, dbcData.State);
                 s1.set(8.0f * size);
             }
             s.set(s1.get() * Math.min(dbcData.Release, 50) / 45);
+        }
+        Entity entity = par1Entity.worldObj.getEntityByID(Integer.parseInt(par1Entity.getmot()));
+        if (entity instanceof EntityCustomNpc) {
+            EntityCustomNpc npc = (EntityCustomNpc) entity;
+            s1.set(8.0f * 0.2f * npc.display.modelSize);
+           s.set(s1.get() * Math.min(par1Entity.getCRel(), 70) / 45);
         }
     }
 }
