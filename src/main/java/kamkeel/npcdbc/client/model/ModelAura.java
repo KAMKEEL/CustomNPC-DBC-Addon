40dc613bcaf4002a08f5419fe27dfb93b89c3660
Kam <KamronGaming@gmail.com>
2024-05-13 16:29:59 -0400

    Aura Tilting

--- src/main/java/kamkeel/npcdbc/client/model/ModelAura.java
+++ src/main/java/kamkeel/npcdbc/client/model/ModelAura.java
@@ -40,12 +40,11 @@ public class ModelAura extends ModelBase {
       //  this.auraModel.rotationPointY = 55.0F + ageTemp;
 
         this.auraModel.offsetY = -ageTemp * 0.175F; // from 0 to -1.64 to -4(max)
-        
-        //from  -0.4  (widest)  to 0.4 
+
+        //from  -0.4  (widest)  to 0.4
        this.auraModel.offsetZ = ageTemp < 8.0F ? 0.3F - ageTemp * 0.1F : -0.5F + (ageTemp - 7.0F) * 0.053F;
         // if (curperc > 0.5)
         //GL11.glScalef(1 - curperc ,1, 1- curperc );
-        this.auraModel.rotateAngleX = 0.8726646F - curperc * 0.01F - rotX;
-    ;
+        this.auraModel.rotateAngleX = (0.8726646F - curperc * 0.01F - rotX) * (1 - ((float) Math.pow(curperc, 2)));
     }
 }
