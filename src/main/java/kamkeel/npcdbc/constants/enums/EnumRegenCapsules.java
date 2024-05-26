a009a8f804a295f8b9d0949c74e6c3528c9c1dbb
somehussar <148253652+somehussar@users.noreply.github.com>
2024-05-26 03:33:03 +0200

    Add nested regen type enum

--- src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
+++ src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
@@ -2,25 +2,33 @@ package kamkeel.npcdbc.constants.enums;
 
 public enum EnumRegenCapsules {
     // @TODO: Add proper configs
-    BaseHP(0, "base_hp", 1, 4, 4),
-    SuperHP(1, "super_hp", 2, 4, 4),
-    MegaHP(2, "mega_hp", 3, 4, 4),
-    BaseKi(3, "base_ki", 1, 4, 4),
-    SuperKi(4, "super_ki", 2, 4, 4),
-    MegaKi(5, "mega_ki", 3, 4, 4),
-    BaseStamina(6, "base_stamina", 1, 4, 4),
-    SuperStamina(7, "super_stamina", 2, 4, 4),
-    MegaStamina(8, "mega_stamina", 3, 4, 4);
+    BaseHP(0, "base_hp", RegenType.HP, 1, 4, 4),
+    SuperHP(1, "super_hp", RegenType.HP, 2, 4, 4),
+    MegaHP(2, "mega_hp", RegenType.HP, 3, 4, 4),
+    BaseKi(3, "base_ki", RegenType.KI, 1, 4, 4),
+    SuperKi(4, "super_ki", RegenType.KI, 2, 4, 4),
+    MegaKi(5, "mega_ki", RegenType.KI, 3, 4, 4),
+    BaseStamina(6, "base_stamina", RegenType.STAMINA, 1, 4, 4),
+    SuperStamina(7, "super_stamina", RegenType.STAMINA, 2, 4, 4),
+    MegaStamina(8, "mega_stamina", RegenType.STAMINA, 3, 4, 4);
+
+    public enum RegenType{
+        HP,
+        KI,
+        STAMINA;
+    }
 
     private final int meta;
     private final String name;
+    private final RegenType regenType;
     private final int strength;
     private final int cooldownType;
     private final int cooldown;
 
-    EnumRegenCapsules(int meta, String name, int strength, int cooldownType, int cooldown){
+    EnumRegenCapsules(int meta, String name, RegenType regenType, int strength, int cooldownType, int cooldown){
         this.meta = meta;
         this.name = name;
+        this.regenType = regenType;
         this.strength = strength;
         this.cooldownType = cooldownType;
         this.cooldown = cooldown;
@@ -31,6 +39,9 @@ public enum EnumRegenCapsules {
 
     public String getName(){
         return this.name;
+    }
+    public RegenType getRegenType() {
+        return this.regenType;
     }
 
     public int getStrength(){
