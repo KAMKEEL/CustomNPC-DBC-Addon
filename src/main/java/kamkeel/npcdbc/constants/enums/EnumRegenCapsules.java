4ffd73ce009d3b17d2ee5085fcae3a05d8f0565a
somehussar <148253652+somehussar@users.noreply.github.com>
2024-05-26 03:25:15 +0200

    Fix meta (someone remind me later to refactor all capsules to use `#ordinal()` later instead of a stupid meta value passed in the constructor

--- src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
+++ src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
@@ -3,14 +3,14 @@ package kamkeel.npcdbc.constants.enums;
 public enum EnumRegenCapsules {
     // @TODO: Add proper configs
     BaseHP(0, "base_hp", 1, 4, 4),
-    SuperHP(0, "super_hp", 2, 4, 4),
-    MegaHP(0, "mega_hp", 3, 4, 4),
-    BaseKi(0, "base_ki", 1, 4, 4),
-    SuperKi(0, "super_ki", 2, 4, 4),
-    MegaKi(0, "mega_ki", 3, 4, 4),
-    BaseStamina(0, "base_stamina", 1, 4, 4),
-    SuperStamina(0, "super_stamina", 2, 4, 4),
-    MegaStamina(0, "mega_stamina", 3, 4, 4);
+    SuperHP(1, "super_hp", 2, 4, 4),
+    MegaHP(2, "mega_hp", 3, 4, 4),
+    BaseKi(3, "base_ki", 1, 4, 4),
+    SuperKi(4, "super_ki", 2, 4, 4),
+    MegaKi(5, "mega_ki", 3, 4, 4),
+    BaseStamina(6, "base_stamina", 1, 4, 4),
+    SuperStamina(7, "super_stamina", 2, 4, 4),
+    MegaStamina(8, "mega_stamina", 3, 4, 4);
 
     private final int meta;
     private final String name;
