bcfa021c52e8610715c428e0b11204246f767c83
Kam <KamronGaming@gmail.com>
2024-05-26 13:33:02 -0400

    Complete Overpower Status Effect

--- src/main/java/kamkeel/npcdbc/controllers/StatusEffectController.java
+++ src/main/java/kamkeel/npcdbc/controllers/StatusEffectController.java
@@ -44,7 +44,7 @@ public class StatusEffectController implements IStatusEffectHandler {
         standardEffects.put(Effects.FRUIT_OF_MIGHT, new FruitOfMight());
         standardEffects.put(Effects.BLOATED, new Bloated());        // TODO: Finish it
         standardEffects.put(Effects.MEDITATION, new Meditation());
-        standardEffects.put(Effects.OVERPOWER, new Overpower());    // TODO: Finish it
+        standardEffects.put(Effects.OVERPOWER, new Overpower());
         standardEffects.put(Effects.CHOCOLATED, new Chocolated());  // TODO: Finish it
         standardEffects.put(Effects.DARKNESS, new Darkness());      // TODO: Finish it
         standardEffects.put(Effects.ZENKAI, new Zenkai());
--- src/main/java/kamkeel/npcdbc/data/statuseffect/types/Overpower.java
+++ src/main/java/kamkeel/npcdbc/data/statuseffect/types/Overpower.java
@@ -2,9 +2,12 @@ package kamkeel.npcdbc.data.statuseffect.types;
 
 import kamkeel.npcdbc.CustomNpcPlusDBC;
 import kamkeel.npcdbc.constants.Effects;
+import kamkeel.npcdbc.controllers.StatusEffectController;
+import kamkeel.npcdbc.data.dbcdata.DBCData;
 import kamkeel.npcdbc.data.statuseffect.PlayerEffect;
 import kamkeel.npcdbc.data.statuseffect.StatusEffect;
 import net.minecraft.entity.player.EntityPlayer;
+import noppes.npcs.util.ValueUtil;
 
 public class Overpower extends StatusEffect {
 
@@ -17,5 +20,15 @@ public class Overpower extends StatusEffect {
     }
 
     @Override
-    public void process(EntityPlayer player, PlayerEffect playerEffect){}
+    public void kill(EntityPlayer player, PlayerEffect playerEffect) {
+        // Ensure Overpower Caps Release back to Default upon Removal
+        DBCData dbcData = DBCData.getData(player);
+        dbcData.loadCharging();
+
+        byte release = dbcData.Release;
+        byte maxRelease = (byte) ((byte) (50 + dbcData.stats.getPotentialUnlockLevel() * 5));
+
+        int newRelease = ValueUtil.clamp(release, (byte) 0, maxRelease);
+        dbcData.getRawCompound().setByte("jrmcRelease", (byte) newRelease);
+    }
 }
