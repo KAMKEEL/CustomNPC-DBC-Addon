e2705a7ca08a9536fc8917fedfabe28a0183ff68
somehussar <148253652+somehussar@users.noreply.github.com>
2024-05-26 03:45:59 +0200

    Move away from RegenType enum

--- src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
+++ src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
@@ -1,34 +1,32 @@
 package kamkeel.npcdbc.constants.enums;
 
+import kamkeel.npcdbc.constants.Effects;
+import kamkeel.npcdbc.controllers.StatusEffectController;
+import kamkeel.npcdbc.data.statuseffect.StatusEffect;
+
 public enum EnumRegenCapsules {
     // @TODO: Add proper configs
-    BaseHP(0, "base_hp", RegenType.HP, 1, 4, 4),
-    SuperHP(1, "super_hp", RegenType.HP, 2, 4, 4),
-    MegaHP(2, "mega_hp", RegenType.HP, 3, 4, 4),
-    BaseKi(3, "base_ki", RegenType.KI, 1, 4, 4),
-    SuperKi(4, "super_ki", RegenType.KI, 2, 4, 4),
-    MegaKi(5, "mega_ki", RegenType.KI, 3, 4, 4),
-    BaseStamina(6, "base_stamina", RegenType.STAMINA, 1, 4, 4),
-    SuperStamina(7, "super_stamina", RegenType.STAMINA, 2, 4, 4),
-    MegaStamina(8, "mega_stamina", RegenType.STAMINA, 3, 4, 4);
-
-    public enum RegenType{
-        HP,
-        KI,
-        STAMINA;
-    }
+    BaseHP(0, "base_hp", Effects.REGEN_HEALTH, 1, 4, 4),
+    SuperHP(1, "super_hp", Effects.REGEN_HEALTH, 2, 4, 4),
+    MegaHP(2, "mega_hp", Effects.REGEN_HEALTH, 3, 4, 4),
+    BaseKi(3, "base_ki", Effects.REGEN_KI, 1, 4, 4),
+    SuperKi(4, "super_ki", Effects.REGEN_KI, 2, 4, 4),
+    MegaKi(5, "mega_ki", Effects.REGEN_KI, 3, 4, 4),
+    BaseStamina(6, "base_stamina", Effects.REGEN_STAMINA, 1, 4, 4),
+    SuperStamina(7, "super_stamina", Effects.REGEN_STAMINA, 2, 4, 4),
+    MegaStamina(8, "mega_stamina", Effects.REGEN_STAMINA, 3, 4, 4);
 
     private final int meta;
     private final String name;
-    private final RegenType regenType;
+    private final int statusEffectId;
     private final int strength;
     private final int cooldownType;
     private final int cooldown;
 
-    EnumRegenCapsules(int meta, String name, RegenType regenType, int strength, int cooldownType, int cooldown){
+    EnumRegenCapsules(int meta, String name, int statusEffectId, int strength, int cooldownType, int cooldown){
         this.meta = meta;
         this.name = name;
-        this.regenType = regenType;
+        this.statusEffectId = statusEffectId;
         this.strength = strength;
         this.cooldownType = cooldownType;
         this.cooldown = cooldown;
@@ -39,9 +37,6 @@ public enum EnumRegenCapsules {
 
     public String getName(){
         return this.name;
-    }
-    public RegenType getRegenType() {
-        return this.regenType;
     }
 
     public int getStrength(){
@@ -54,6 +49,9 @@ public enum EnumRegenCapsules {
 
     public int getCooldown(){
         return this.cooldown;
+    }
+    public int getStatusEffect(){
+        return statusEffectId;
     }
 
     public static int count(){
