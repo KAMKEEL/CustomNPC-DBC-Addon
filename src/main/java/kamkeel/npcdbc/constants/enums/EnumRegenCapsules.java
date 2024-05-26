ac1e5ec3e64d7144aa593dc9b7ea197017f08e19
somehussar <148253652+somehussar@users.noreply.github.com>
2024-05-26 03:55:21 +0200

    Change enum structure & add item functionality

--- src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
+++ src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
@@ -1,33 +1,33 @@
 package kamkeel.npcdbc.constants.enums;
 
 import kamkeel.npcdbc.constants.Effects;
-import kamkeel.npcdbc.controllers.StatusEffectController;
-import kamkeel.npcdbc.data.statuseffect.StatusEffect;
 
 public enum EnumRegenCapsules {
     // @TODO: Add proper configs
-    BaseHP(0, "base_hp", Effects.REGEN_HEALTH, 1, 4, 4),
-    SuperHP(1, "super_hp", Effects.REGEN_HEALTH, 2, 4, 4),
-    MegaHP(2, "mega_hp", Effects.REGEN_HEALTH, 3, 4, 4),
-    BaseKi(3, "base_ki", Effects.REGEN_KI, 1, 4, 4),
-    SuperKi(4, "super_ki", Effects.REGEN_KI, 2, 4, 4),
-    MegaKi(5, "mega_ki", Effects.REGEN_KI, 3, 4, 4),
-    BaseStamina(6, "base_stamina", Effects.REGEN_STAMINA, 1, 4, 4),
-    SuperStamina(7, "super_stamina", Effects.REGEN_STAMINA, 2, 4, 4),
-    MegaStamina(8, "mega_stamina", Effects.REGEN_STAMINA, 3, 4, 4);
+    BaseHP(0, "base_hp", Effects.REGEN_HEALTH, (byte) 1, 30, 4, 4),
+    SuperHP(1, "super_hp", Effects.REGEN_HEALTH, (byte) 2, 20, 4, 4),
+    MegaHP(2, "mega_hp", Effects.REGEN_HEALTH, (byte) 3, 10, 4, 4),
+    BaseKi(3, "base_ki", Effects.REGEN_KI, (byte) 1, 30, 4, 4),
+    SuperKi(4, "super_ki", Effects.REGEN_KI, (byte) 2, 20, 4, 4),
+    MegaKi(5, "mega_ki", Effects.REGEN_KI, (byte) 3, 10, 4, 4),
+    BaseStamina(6, "base_stamina", Effects.REGEN_STAMINA, (byte) 1, 30, 4, 4),
+    SuperStamina(7, "super_stamina", Effects.REGEN_STAMINA, (byte) 2, 20, 4, 4),
+    MegaStamina(8, "mega_stamina", Effects.REGEN_STAMINA, (byte) 3, 10, 4, 4);
 
     private final int meta;
     private final String name;
     private final int statusEffectId;
-    private final int strength;
+    private final byte strength;
+    private final int useTime;
     private final int cooldownType;
     private final int cooldown;
 
-    EnumRegenCapsules(int meta, String name, int statusEffectId, int strength, int cooldownType, int cooldown){
+    EnumRegenCapsules(int meta, String name, int statusEffectId, byte strength, int useTime, int cooldownType, int cooldown){
         this.meta = meta;
         this.name = name;
         this.statusEffectId = statusEffectId;
         this.strength = strength;
+        this.useTime = useTime;
         this.cooldownType = cooldownType;
         this.cooldown = cooldown;
     }
@@ -39,8 +39,8 @@ public enum EnumRegenCapsules {
         return this.name;
     }
 
-    public int getStrength(){
-        return this.strength;
+    public byte getStrength(){
+        return (byte) this.strength;
     }
 
     public int getCooldownType() {
@@ -50,11 +50,15 @@ public enum EnumRegenCapsules {
     public int getCooldown(){
         return this.cooldown;
     }
-    public int getStatusEffect(){
+    public int getStatusEffectId(){
         return statusEffectId;
     }
 
     public static int count(){
         return values().length;
+    }
+
+    public int getUseTime() {
+        return this.useTime;
     }
 }
