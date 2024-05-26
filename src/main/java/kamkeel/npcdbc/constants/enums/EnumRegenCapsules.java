45657d2305cd3078d7f1a01c2bcb19349ff70e92
somehussar <148253652+somehussar@users.noreply.github.com>
2024-05-26 03:20:03 +0200

    Add missing capsule enums

--- src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
+++ src/main/java/kamkeel/npcdbc/constants/enums/EnumRegenCapsules.java
@@ -2,9 +2,15 @@ package kamkeel.npcdbc.constants.enums;
 
 public enum EnumRegenCapsules {
     // @TODO: Add proper configs
-    BaseHP(0, "base_hp", 2, 4, 4);
-
-    ;
+    BaseHP(0, "base_hp", 1, 4, 4),
+    SuperHP(0, "super_hp", 2, 4, 4),
+    MegaHP(0, "mega_hp", 3, 4, 4),
+    BaseKi(0, "base_ki", 1, 4, 4),
+    SuperKi(0, "super_ki", 2, 4, 4),
+    MegaKi(0, "mega_ki", 3, 4, 4),
+    BaseStamina(0, "base_stamina", 1, 4, 4),
+    SuperStamina(0, "super_stamina", 2, 4, 4),
+    MegaStamina(0, "mega_stamina", 3, 4, 4);
 
     private final int meta;
     private final String name;
