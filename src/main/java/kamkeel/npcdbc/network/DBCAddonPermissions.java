8c15246b9ab85ca695a2b66e51faea16e765f2a1
Kam <KamronGaming@gmail.com>
2024-05-28 05:59:19 -0400

    Add Permission Check to SaveForm()

---
+++ src/main/java/kamkeel/npcdbc/network/DBCAddonPermissions.java
@@ -0,0 +1,8 @@
+package kamkeel.npcdbc.network;
+
+import noppes.npcs.CustomNpcsPermissions;
+
+public class DBCAddonPermissions {
+    public static final CustomNpcsPermissions.Permission GLOBAL_CUSTOMFORM = new CustomNpcsPermissions.Permission("customnpcs.global.customform");
+
+}
