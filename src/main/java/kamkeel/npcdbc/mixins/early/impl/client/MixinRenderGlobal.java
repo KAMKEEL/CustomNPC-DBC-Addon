5ba8840c77305586dadc33d7bad78dd1c1e59b9f
bigguy345 <monkeybar3200@gmail.com>
2024-06-27 21:43:32 +0300

    MixinRenderGlobal null pointer

--- src/main/java/kamkeel/npcdbc/mixins/early/impl/client/MixinRenderGlobal.java
+++ src/main/java/kamkeel/npcdbc/mixins/early/impl/client/MixinRenderGlobal.java
@@ -2,6 +2,7 @@ package kamkeel.npcdbc.mixins.early.impl.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import kamkeel.npcdbc.client.ClientProxy;
 import kamkeel.npcdbc.util.Utility;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
@@ -38,25 +39,35 @@ public class MixinRenderGlobal {
 
     @Redirect(method = "renderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/WorldClient;getLoadedEntityList()Ljava/util/List;"))
     private List secondRendPass(WorldClient instance, @Local(ordinal = 0) LocalRef<ICamera> camera) {
+        System.out.println("aaaa1");
         sorted = MinecraftForgeClient.getRenderPass() != 0; //sorts only once per tick, at the start of rend pass 0
+
         if (!sorted) {
-            sortedEntityList = new ArrayList(instance.getLoadedEntityList());
-            ClippingHelperImpl frustum = (ClippingHelperImpl) Utility.getPFValue(ClippingHelperImpl.class, "instance", null);
-            float[] nearPlane = frustum.frustum[5];
-
-            float planeX = (float) (RenderManager.renderPosX - nearPlane[3] * nearPlane[0]); //posX of the near plane center
-            float planeY = (float) (RenderManager.renderPosY - nearPlane[3] * nearPlane[1]); //posY
-            float planeZ = (float) (RenderManager.renderPosZ - nearPlane[3] * nearPlane[2]); //posZ
-
-            Collections.sort(sortedEntityList, (Comparator<Entity>) (entity1, entity2) -> {
-                double distanceToEntity1 = entity1.getDistanceSq(planeX, planeY, planeZ);
-                double distanceToEntity2 = entity2.getDistanceSq(planeX, planeY, planeZ);
-                return Double.compare(distanceToEntity2, distanceToEntity1); // Sorting from furthest to nearest
-            });
-            sorted = true;
+            try {
+                sortedEntityList = new ArrayList(instance.getLoadedEntityList());
+                System.out.println("aaaa2");
+                ClippingHelperImpl frustum = (ClippingHelperImpl) Utility.getPFValue(ClippingHelperImpl.class, "instance", null);
+                System.out.println("aaaa3");
+                float[] nearPlane = frustum.frustum[5];
+                System.out.println("aaaa4");
+                float planeX = (float) (RenderManager.renderPosX - nearPlane[3] * nearPlane[0]); //posX of the near plane center
+                float planeY = (float) (RenderManager.renderPosY - nearPlane[3] * nearPlane[1]); //posY
+                float planeZ = (float) (RenderManager.renderPosZ - nearPlane[3] * nearPlane[2]); //posZ
+                System.out.println("aaaa5");
+                Collections.sort(sortedEntityList, (Comparator<Entity>) (entity1, entity2) -> {
+                    double distanceToEntity1 = entity1.getDistanceSq(planeX, planeY, planeZ);
+                    double distanceToEntity2 = entity2.getDistanceSq(planeX, planeY, planeZ);
+                    return Double.compare(distanceToEntity2, distanceToEntity1); // Sorting from furthest to nearest
+                });
+                System.out.println("aaaa6");
+                sorted = true;
+                System.out.println("aaaa7");
+            } catch (Exception e) {
+                ClientProxy.LOGGER.error("Failed to sort entities: " + e.getMessage());
+            }
         }
 
-        if (sortedEntityList != null)
+        if (sorted && sortedEntityList != null)
             return sortedEntityList;
         else
             return instance.getLoadedEntityList();
