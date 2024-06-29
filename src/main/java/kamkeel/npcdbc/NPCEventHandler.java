60fde1f480d55198659241f82e398923e9074097
Kam <KamronGaming@gmail.com>
2024-06-29 14:16:55 -0400

    Quick fix

--- src/main/java/kamkeel/npcdbc/NPCEventHandler.java
+++ src/main/java/kamkeel/npcdbc/NPCEventHandler.java
@@ -4,16 +4,17 @@ import kamkeel.npcdbc.data.dbcdata.DBCData;
 import kamkeel.npcdbc.data.form.Form;
 import kamkeel.npcdbc.scripted.DBCPlayerEvent;
 import kamkeel.npcdbc.util.PlayerDataUtil;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class NPCEventHandler {
 
     public void DBCAttackEvent(DBCPlayerEvent.DamagedEvent event) {
-        if (event.player == null || event.player == null || event.player.worldObj.isRemote)
+        if (event.player == null || event.player == null || event.player.getMCEntity() == null || event.player.getMCEntity().worldObj.isRemote)
             return;
 
-        Form form = DBCData.getForm(event.player);
+        Form form = DBCData.getForm((EntityPlayer) event.player.getMCEntity());
         if (form != null) {
-            float formLevel = PlayerDataUtil.getFormLevel(event.player);
+            float formLevel = PlayerDataUtil.getFormLevel(event.player.getMCEntity());
 
             if (form.mastery.hasDamageNegation()) {
                 float damageNegation = form.mastery.damageNegation * form.mastery.calculateMulti("damageNegation", formLevel);
