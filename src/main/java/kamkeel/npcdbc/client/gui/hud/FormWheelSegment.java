fa33a0108e5cd587b5e76007624838e6d92254c2
bigguy345 <monkeybar3200@gmail.com>
2024-07-31 15:28:46 +0300

    Moved segment rendering data from FormWheelSegment into WheelSegment

--- src/main/java/kamkeel/npcdbc/client/gui/hud/FormWheelSegment.java
+++ src/main/java/kamkeel/npcdbc/client/gui/hud/FormWheelSegment.java
@@ -1,6 +1,5 @@
 package kamkeel.npcdbc.client.gui.hud;
 
-import kamkeel.npcdbc.client.utils.Color;
 import kamkeel.npcdbc.controllers.FormController;
 import kamkeel.npcdbc.data.form.Form;
 import kamkeel.npcdbc.network.PacketHandler;
@@ -9,19 +8,9 @@ import kamkeel.npcdbc.network.packets.form.DBCSelectForm;
 import net.minecraft.client.Minecraft;
 
 class FormWheelSegment extends WheelSegment {
-
     public HUDFormWheel parent;
     public Form form;
     public int formID;
-
-    public static Color HOVERED = new Color(0xADD8E6, 0.7f);
-    public static Color NOT_HOVERED = new Color(0xFFFFFF, 0.35f);
-
-    private boolean isHovered = false;
-    private long startHoverTime = 0;
-    private long stopHoverTime = 0;
-    private float hoverScale = 0;
-    private Color currentColor;
 
     FormWheelSegment(HUDFormWheel parent, int index) {
         this(parent, 0, 0, index);
@@ -35,20 +24,10 @@ class FormWheelSegment extends WheelSegment {
         this.posY = posY;
         this.index = index;
 
-        this.currentColor = NOT_HOVERED;
     }
-
-    @Override
-    public void draw() {
-        currentColor = Color.lerpRGBA(NOT_HOVERED, HOVERED, hoverScale);
-        currentColor.glColor();
-        super.draw();
-    }
-
 
     public void selectForm() {
         PacketHandler.Instance.sendToServer(new DBCSelectForm(formID).generatePacket());
-
     }
 
     public void setForm(int formID, boolean updateServer) {
@@ -69,32 +48,5 @@ class FormWheelSegment extends WheelSegment {
 
     }
 
-    public void setHoveredState(boolean newHoverState) {
-
-        if (!isHovered && newHoverState) {
-            startHoverTime = Minecraft.getSystemTime();
-        }
-        if (isHovered && !newHoverState) {
-            stopHoverTime = Minecraft.getSystemTime();
-        }
-        isHovered = newHoverState;
-    }
-
-    public float getSegmentScale() {
-        float updateTime;
-        if (isHovered) {
-            updateTime = (float) (Minecraft.getSystemTime() - startHoverTime) / 100;
-            updateTime = Math.min(updateTime, 1);
-            hoverScale = (hoverScale + 0.25f * (updateTime - hoverScale));
-        } else {
-            updateTime = (float) (Minecraft.getSystemTime() - stopHoverTime) / 100;
-            updateTime = -Math.min(updateTime, 1);
-            hoverScale = (hoverScale + 0.25f * (1 + updateTime - hoverScale));
-        }
-
-        hoverScale = Math.min(1, Math.max(hoverScale, 0));
-
-        return hoverScale;
-    }
 
 }
--- src/main/java/kamkeel/npcdbc/client/gui/hud/WheelSegment.java
+++ src/main/java/kamkeel/npcdbc/client/gui/hud/WheelSegment.java
@@ -1,6 +1,7 @@
 package kamkeel.npcdbc.client.gui.hud;
 
 import kamkeel.npcdbc.CustomNpcPlusDBC;
+import kamkeel.npcdbc.client.utils.Color;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.Tessellator;
@@ -11,16 +12,27 @@ class WheelSegment extends Gui {
     public static ResourceLocation variant1 = new ResourceLocation(CustomNpcPlusDBC.ID + ":textures/gui/hud/formwheel/GuiWheelVariant1.png");
     public static ResourceLocation variant2 = new ResourceLocation(CustomNpcPlusDBC.ID + ":textures/gui/hud/formwheel/GuiWheelVariant2.png");
 
+    public static Color HOVERED = new Color(0xADD8E6, 0.65f);
+    public static Color NOT_HOVERED = new Color(0xFFFFFF, 0.35f);
+
     public static double width = 124;
     public static double height = 124;
     public static double f = 124.0 / 744;
 
     public static int variant = 0;
 
+    public Color currentColor;
+
     public int posX;
     public int posY;
     public int index;
     public boolean selected;
+
+    public boolean isHovered = false;
+    public long startHoverTime = 0;
+    public long stopHoverTime = 0;
+    public float hoverScale = 0;
+
 
     WheelSegment(int index) {
         this(0, 0, index);
@@ -33,7 +45,37 @@ class WheelSegment extends Gui {
         this.index = index;
     }
 
+    public void setHoveredState(boolean newHoverState) {
+
+        if (!isHovered && newHoverState) {
+            startHoverTime = Minecraft.getSystemTime();
+        }
+        if (isHovered && !newHoverState) {
+            stopHoverTime = Minecraft.getSystemTime();
+        }
+        isHovered = newHoverState;
+    }
+
+    public float getSegmentScale() {
+        float updateTime;
+        if (isHovered) {
+            updateTime = (float) (Minecraft.getSystemTime() - startHoverTime) / 100;
+            updateTime = Math.min(updateTime, 1);
+            hoverScale = (hoverScale + 0.25f * (updateTime - hoverScale));
+        } else {
+            updateTime = (float) (Minecraft.getSystemTime() - stopHoverTime) / 100;
+            updateTime = -Math.min(updateTime, 1);
+            hoverScale = (hoverScale + 0.25f * (1 + updateTime - hoverScale));
+        }
+
+        hoverScale = Math.min(1, Math.max(hoverScale, 0));
+
+        return hoverScale;
+    }
+
     public void draw() {
+        currentColor =Color.lerpRGBA(NOT_HOVERED, HOVERED, hoverScale);
+        currentColor.glColor();
         drawIndexedTexture();
     }
 
