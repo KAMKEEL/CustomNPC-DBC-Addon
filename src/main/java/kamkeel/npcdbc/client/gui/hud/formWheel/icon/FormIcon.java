c7f75b68732b62fd369943c82f8d7078f84bde09
somehussar <148253652+somehussar@users.noreply.github.com>
2024-08-13 08:53:57 +0200

    Set up DBC form support for saiyan/namek/human

--- src/main/java/kamkeel/npcdbc/client/gui/hud/formWheel/icon/FormIcon.java
+++ src/main/java/kamkeel/npcdbc/client/gui/hud/formWheel/icon/FormIcon.java
@@ -16,11 +16,13 @@ import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class FormIcon extends Gui {
-    public ResourceLocation bodyHairIcon = new ResourceLocation(CustomNpcPlusDBC.ID + ":textures/gui/hud/formwheel/icon/humanoid/body1.png");
-    public ResourceLocation auraIcon = new ResourceLocation(CustomNpcPlusDBC.ID + ":textures/gui/hud/formwheel/icon/aurapng");
+    public static final String PREFIX = CustomNpcPlusDBC.ID + ":textures/gui/hud/formwheel/icon";
+
+    public ResourceLocation bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/normal.png");
+    public ResourceLocation auraIcon = new ResourceLocation(PREFIX + "/aura.png");
     public HUDFormWheel parent;
 
-    public Color hairColor = new Color(0x2ACDEE, 1).lerpRGBA(new Color(0, 1), 0.2f), auraColor = new Color(0x2ACDEE, 1);
+    public Color hairColor = new Color(0, 1), auraColor = new Color(0xADD8E6, 1);
     public int width = 16, height = 16;
 
     public FormIcon(HUDFormWheel parent, Form formToCopy){
@@ -28,6 +30,32 @@ public class FormIcon extends Gui {
     }
     public FormIcon(HUDFormWheel parent, int DBCFormID){
         this.parent = parent;
+        boolean isLegendary = parent.dbcData.isForm(DBCForm.Legendary);
+        boolean isDivine = parent.dbcData.isForm(DBCForm.Divine);
+
+        if(DBCFormID >= 20){
+            setNonRacialForms(DBCFormID);
+        }
+
+        switch(parent.dbcData.Race){
+            case 0:
+                setHumanForms(DBCFormID);
+                break;
+            case 1:
+            case 2:
+                setSaiyanForms(DBCFormID, isLegendary, isDivine);
+                break;
+            case 3:
+                setNamekForms(DBCFormID);
+                break;
+            case 4:
+                setArcoForms(DBCFormID);
+                break;
+            case 5:
+                setMajinForms(DBCFormID);
+                break;
+        }
+
     }
 
     public void draw(){
@@ -66,6 +94,99 @@ public class FormIcon extends Gui {
         tessellator.draw();
     }
 
+    private void setNonRacialForms(int state) {
 
+    }
 
+    private void setHumanForms(int state){
+        hairColor = new Color(0, 1);
+        auraColor = new Color(0xADD8E6, 1);
+        auraIcon = new ResourceLocation(PREFIX + "/aura.png");
+        bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/normal.png");
+        if(state == DBCForm.HumanBuffed){
+            auraColor = new Color(0xFCE892, 1);
+            bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/buff.png");
+        }
+        if(state == DBCForm.HumanGod){
+            auraIcon = new ResourceLocation(PREFIX + "/auraGod.png");
+            auraColor = new Color(0xFFC125, 1);
+        }
+    }
+    private void setSaiyanForms(int state, boolean isLegendary, boolean isDivine){
+        Color basicSSJAuraColor = new Color(isLegendary ? 0x99FF66 : 0xFCE892, 1);
+        Color basicSSJHairColor = basicSSJAuraColor.lerpRGBA(new Color(0, 1), 0.2f);
+
+        Color ssbAuraColor = new Color(isDivine ? 0x730015 : 0x2ACDEE, 1);
+        Color ssbHairColor = isDivine ? new Color(0xE4A8AE, 1) : ssbAuraColor.lerpRGBA(new Color(0, 1), 0.2f);
+
+        bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/normal.png");
+        auraIcon = new ResourceLocation(PREFIX + "/aura.png");
+        auraColor = new Color(0xADD8E6, 1);
+        hairColor = new Color(0, 1);
+        switch(state){
+            case DBCForm.SuperSaiyanG2:
+            case DBCForm.SuperSaiyanG3:
+                bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/buff.png");
+                auraColor = basicSSJAuraColor;
+                hairColor = basicSSJHairColor;
+                break;
+            case DBCForm.SuperSaiyan:
+            case DBCForm.MasteredSuperSaiyan:
+            case DBCForm.SuperSaiyan2:
+            case DBCForm.SuperSaiyan3:
+                auraColor = basicSSJAuraColor;
+                hairColor = basicSSJHairColor;
+                break;
+            case DBCForm.GreatApe:
+                bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/oozaru.png");
+                hairColor = new Color(0x42280E, 1);
+                break;
+            case DBCForm.SuperGreatApe:
+                bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/oozaru.png");
+                auraColor = basicSSJAuraColor;
+                hairColor = basicSSJHairColor;
+                break;
+            case DBCForm.SuperSaiyanGod:
+                auraIcon = new ResourceLocation(PREFIX + "/auraGod.png");
+                auraColor = new Color(0xFFC125, 1);
+                hairColor = new Color(0xD60D6B, 1);
+                break;
+            case DBCForm.SuperSaiyanBlue:
+                auraIcon = new ResourceLocation(PREFIX + "/auraGod.png");
+                auraColor = ssbAuraColor;
+                hairColor = ssbHairColor;
+                break;
+            case DBCForm.BlueEvo:
+                auraIcon = new ResourceLocation(PREFIX + "/auraGod.png");
+                bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/buff.png");
+                auraColor = isDivine ? ssbAuraColor.lerpRGBA(new Color(0xFC6C85, 1), 0.6f) : new Color(0x2233FF, 1);
+                hairColor = isDivine ? ssbHairColor.lerpRGBA(new Color(0x090909, 1), 0.2f) : new Color(0x2233FF, 1).lerpRGBA(new Color(0, 1), 0.15f);
+                break;
+            case DBCForm.SuperSaiyan4:
+                bodyHairIcon = new ResourceLocation(PREFIX + "/humanoid/ss4.png");
+                hairColor = new Color(0, 1);
+                auraColor = basicSSJAuraColor;
+                break;
+        }
+
+    }
+    private void setNamekForms(int state){
+        hairColor = new Color(0x6DB43A, 1);
+        auraColor = new Color(0xADD8E6, 1);
+        auraIcon = new ResourceLocation(PREFIX + "/aura.png");
+        bodyHairIcon = new ResourceLocation(PREFIX + "/namek/body.png");
+        if(state == DBCForm.NamekGiant){
+            auraColor = new Color(0xFCE892, 1);
+        }
+        if(state == DBCForm.NamekGod){
+            auraIcon = new ResourceLocation(PREFIX + "/auraGod.png");
+            auraColor = new Color(0xFFC125, 1);
+        }
+    }
+    private void setArcoForms(int state){
+
+    }
+    private void setMajinForms(int state){
+
+    }
 }
