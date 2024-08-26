4454e37737add25a0fd73b6cada7acfc139d220e
bigguy345 <monkeybar3200@gmail.com>
2024-08-26 04:44:19 +0300

    ModernGL unsupported fix

--- src/main/java/kamkeel/npcdbc/client/ClientProxy.java
+++ src/main/java/kamkeel/npcdbc/client/ClientProxy.java
@@ -62,7 +62,7 @@ public class ClientProxy extends CommonProxy {
 
     public void postInit(FMLPostInitializationEvent ev) {
         PostProcessing.init(Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
-        ModernModels.loadModels();
+      //  ModernModels.loadModels();
     }
 
     public static float getTimeSinceStart() {
