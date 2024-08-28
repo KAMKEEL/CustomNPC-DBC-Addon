6c92a084d52fc59684314c38db57a2d5eac883a7
somehussar <148253652+somehussar@users.noreply.github.com>
2024-08-28 03:59:37 +0200

    1.0-24w35-01

--- src/main/java/kamkeel/npcdbc/CustomNpcPlusDBC.java
+++ src/main/java/kamkeel/npcdbc/CustomNpcPlusDBC.java
@@ -19,7 +19,7 @@ import java.io.File;
 public class CustomNpcPlusDBC {
 
     public static final String name = "CustomNPC+ DBC Addon";
-    public static final String version = "1.0-Release_Candidate_2";
+    public static final String version = "1.0-24w35-01";
     public static final String ID = "npcdbc";
     @SidedProxy(clientSide = "kamkeel.npcdbc.client.ClientProxy", serverSide = "kamkeel.npcdbc.CommonProxy")
     public static CommonProxy proxy;
