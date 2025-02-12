9cb104b55fbf202a0be6c0213d103ad481503b46
lonewolf8351 <122708538+lonewolf8351@users.noreply.github.com>
2025-02-11 22:08:58 -0500

    Gave choclated effect some oomf

--- src/main/java/kamkeel/npcdbc/data/effects/types/Chocolated.java
+++ src/main/java/kamkeel/npcdbc/data/effects/types/Chocolated.java
@@ -1,11 +1,17 @@
 package kamkeel.npcdbc.data.effects.types;
 
+import kamkeel.npcdbc.config.ConfigDBCEffects;
 import kamkeel.npcdbc.constants.Effects;
+import kamkeel.npcdbc.controllers.BonusController;
+import kamkeel.npcdbc.data.PlayerBonus;
 import kamkeel.npcdbc.data.effects.AddonEffect;
 import net.minecraft.entity.player.EntityPlayer;
 import noppes.npcs.controllers.data.PlayerEffect;
+import noppes.npcs.scripted.event.PlayerEvent;
 
 public class Chocolated extends AddonEffect {
+
+public PlayerBonus ChocolatedDebuff;
 
     public Chocolated() {
         name = "Chocolated";
@@ -13,8 +19,21 @@ public class Chocolated extends AddonEffect {
         id = Effects.CHOCOLATED;
         iconX = 96;
         iconY = 0;
+        ChocolatedDebuff = new PlayerBonus(name, (byte) 0, (float) ConfigDBCEffects.CHOC_Str, (float) ConfigDBCEffects.CHOC_Dex, (float) ConfigDBCEffects.CHOC_Wil);
+        length = ConfigDBCEffects.CHOC_EffectLength;
+    }
+
+    @Override
+    public void onAdded(EntityPlayer player, PlayerEffect playerEffect){
+        BonusController.getInstance().applyBonus(player,ChocolatedDebuff);
     }
 
     @Override
     public void onTick(EntityPlayer player, PlayerEffect playerEffect){}
+
+    @Override
+    public void onRemoved(EntityPlayer player, PlayerEffect playerEffect, PlayerEvent.EffectEvent.ExpirationType type){
+        BonusController.getInstance().removeBonus(player,ChocolatedDebuff);
+    }
+
 }
