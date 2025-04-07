65aa07f530cf6dc78bf480c1210fe43d52dd7aea
Kam <KamronGaming@gmail.com>
2025-04-07 17:44:56 -0400

    Add DBC Stats Constants

---
+++ src/main/java/kamkeel/npcdbc/constants/DBCStats.java
@@ -0,0 +1,16 @@
+package kamkeel.npcdbc.constants;
+
+public class DBCStats {
+    public static final int Melee = 0;
+    public static final int Defense = 1;
+    public static final int Body = 2;
+    public static final int Stamina = 3;
+    public static final int EnergyPower = 4;
+    public static final int EnergyPool = 5;
+    public static final int MaxSkills = 6;
+    public static final int Speed = 7;
+    public static final int RegenRateBody = 8;
+    public static final int RegenRateStamina = 9;
+    public static final int RegenRateEnergy = 10;
+    public static final int FlySpeed = 11;
+}
