5f6c308bd820b8be1f684ff07ff40c28a7185629
Kam <KamronGaming@gmail.com>
2025-04-09 19:44:01 -0400

    For Individual Player Script Handling

--- src/main/java/kamkeel/npcdbc/scripted/DBCEventHooks.java
+++ src/main/java/kamkeel/npcdbc/scripted/DBCEventHooks.java
@@ -8,37 +8,37 @@ import noppes.npcs.scripted.NpcAPI;
 public class DBCEventHooks {
 
     public static boolean onFormChangeEvent(DBCPlayerEvent.FormChangeEvent formChangeEvent) {
-        PlayerDataScript handler = ScriptController.Instance.playerScripts;
+        PlayerDataScript handler = ScriptController.Instance.getPlayerScripts(formChangeEvent.getPlayer());
         handler.callScript(DBCScriptType.FORMCHANGE.function, formChangeEvent);
         return NpcAPI.EVENT_BUS.post(formChangeEvent);
     }
 
     public static boolean onDBCDamageEvent(DBCPlayerEvent.DamagedEvent damagedEvent) {
-        PlayerDataScript handler = ScriptController.Instance.playerScripts;
+        PlayerDataScript handler = ScriptController.Instance.getPlayerScripts(damagedEvent.getPlayer());
         handler.callScript(DBCScriptType.DAMAGED.function, damagedEvent);
         return NpcAPI.EVENT_BUS.post(damagedEvent);
     }
 
     public static boolean onCapsuleUsedEvent(DBCPlayerEvent.CapsuleUsedEvent capsuleUsedEvent) {
-        PlayerDataScript handler = ScriptController.Instance.playerScripts;
+        PlayerDataScript handler = ScriptController.Instance.getPlayerScripts(capsuleUsedEvent.getPlayer());
         handler.callScript(DBCScriptType.CAPSULEUSED.function, capsuleUsedEvent);
         return NpcAPI.EVENT_BUS.post(capsuleUsedEvent);
     }
 
     public static boolean onSenzuUsedEvent(DBCPlayerEvent.SenzuUsedEvent senzuUsedEvent) {
-        PlayerDataScript handler = ScriptController.Instance.playerScripts;
+        PlayerDataScript handler = ScriptController.Instance.getPlayerScripts(senzuUsedEvent.getPlayer());
         handler.callScript(DBCScriptType.SENZUUSED.function, senzuUsedEvent);
         return NpcAPI.EVENT_BUS.post(senzuUsedEvent);
     }
 
     public static boolean onReviveEvent(DBCPlayerEvent.ReviveEvent reviveEvent) {
-        PlayerDataScript handler = ScriptController.Instance.playerScripts;
+        PlayerDataScript handler = ScriptController.Instance.getPlayerScripts(reviveEvent.getPlayer());
         handler.callScript(DBCScriptType.REVIVED.function, reviveEvent);
         return NpcAPI.EVENT_BUS.post(reviveEvent);
     }
 
     public static boolean onKnockoutEvent(DBCPlayerEvent.KnockoutEvent koEvent) {
-        PlayerDataScript handler = ScriptController.Instance.playerScripts;
+        PlayerDataScript handler = ScriptController.Instance.getPlayerScripts(koEvent.getPlayer());
         handler.callScript(DBCScriptType.KNOCKOUT.function, koEvent);
         return NpcAPI.EVENT_BUS.post(koEvent);
     }
