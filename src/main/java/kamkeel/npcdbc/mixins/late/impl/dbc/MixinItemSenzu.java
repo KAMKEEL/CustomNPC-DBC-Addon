d7ff13e0f9b5611855f6ab0ca1dbd0facef8a605
Kam <KamronGaming@gmail.com>
2025-07-25 14:22:21 -0400

    Fix Senzu Used Event

--- src/main/java/kamkeel/npcdbc/mixins/late/impl/dbc/MixinItemSenzu.java
+++ src/main/java/kamkeel/npcdbc/mixins/late/impl/dbc/MixinItemSenzu.java
@@ -13,25 +13,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ItemSenzu.class, remap = false)
+@Mixin(value = ItemSenzu.class)
 public class MixinItemSenzu {
 
-    @Inject(method = "onPlayerStoppedUsing",
-        at = @At(value = "INVOKE", target = "LJinRyuu/JRMCore/server/JGPlayerMP;getAttributes()[I", shift = At.Shift.BEFORE),
-        remap = false, cancellable = true)
+    @Inject(method = "onPlayerStoppedUsing", at = @At(value = "INVOKE", target = "LJinRyuu/JRMCore/server/JGPlayerMP;getAttributes()[I", shift = At.Shift.BEFORE), cancellable = true)
     public void callSenzuEvent(ItemStack itemStack, World world, EntityPlayer entityPlayer, int par4, CallbackInfo ci) {
         if (DBCEventHooks.onSenzuUsedEvent(new DBCPlayerEvent.SenzuUsedEvent(PlayerDataUtil.getIPlayer(entityPlayer))))
             ci.cancel();
 
         if (!DBCEffectController.getInstance().allowSenzuConsumption(entityPlayer))
-            ci.cancel();
-    }
-
-    @Inject(method = "onPlayerStoppedUsing",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/util/FoodStats;addStats(IF)V", shift = At.Shift.AFTER),
-        remap = false, cancellable = true)
-    public void countSenzuConsume(ItemStack itemStack, World world, EntityPlayer entityPlayer, int par4, CallbackInfo ci) {
-        if (DBCEventHooks.onSenzuUsedEvent(new DBCPlayerEvent.SenzuUsedEvent(PlayerDataUtil.getIPlayer(entityPlayer))))
             ci.cancel();
     }
 }
