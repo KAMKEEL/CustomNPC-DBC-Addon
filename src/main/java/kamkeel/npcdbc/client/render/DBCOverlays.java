f7679e2cfd1047598f697ffa2330ca359a6bc3df
bigguy345 <monkeybar3200@gmail.com>
2025-12-14 13:24:27 +0200

    Cleaned up DBCOverlays with new TexturePath function interface

--- src/main/java/kamkeel/npcdbc/client/render/DBCOverlays.java
+++ src/main/java/kamkeel/npcdbc/client/render/DBCOverlays.java
@@ -2,6 +2,7 @@ package kamkeel.npcdbc.client.render;
 
 import kamkeel.npcdbc.data.form.FacePartData.Part;
 import kamkeel.npcdbc.data.overlay.OverlayChain;
+import kamkeel.npcdbc.data.overlay.OverlayContext;
 
 import static java.lang.String.format;
 import static kamkeel.npcdbc.client.model.ModelDBC.*;
@@ -12,37 +13,48 @@ import static kamkeel.npcdbc.data.overlay.Overlay.Type.*;
 public class DBCOverlays {
 
     public static final OverlayChain SSJ4_FUR = OverlayChain.create("SSJ4 Fur");
-    public static final OverlayChain OOZARU_FUR = OverlayChain.create("Oozaru");
-    public static final OverlayChain SSJ4_FACE = OverlayChain.create("SSJ4 Face");
+    public static final OverlayChain OOZARU_FUR = OverlayChain.create("Oozaru Fur");
+
     public static final OverlayChain SSJ3_FACE = OverlayChain.create("SSJ3 Face");
+    public static final OverlayChain SSJ4_FACE = OverlayChain.create("SSJ4 Face");
+
     public static final OverlayChain PUPILS = OverlayChain.create("Pupils");
 
     static {
-        SSJ4_FUR.add(ALL, Fur, (ctx) -> path("ssj4/ss4b" + ctx.furType() + ".png", "jinryuudragonbc:cc/ss4b"));
+        SSJ4_FUR.add(ALL, Fur, ctx -> path("ssj4/ss4b" + ctx.furType() + ".png", "jinryuudragonbc:cc/ss4b"));
 
 
         OOZARU_FUR.add(ALL, BodyCM, path("oozaru/oozaru1.png", "jinryuudragonbc:cc/oozaru1.png"));
         OOZARU_FUR.add(ALL, Fur, path("oozaru/oozaru2.png", "jinryuudragonbc:cc/oozaru2.png"));
 
 
-        SSJ4_FACE.add(Face, BodyCM, (ctx) -> format(HD("%s/%s/face_%s/ssj4shade.png"), ctx.furDir(), ctx.genderDir(), ctx.eyeType()));
-        SSJ4_FACE.add(EyeWhite, 0xFFFFFF, (ctx) -> format(HD("%s/%s/face_%s/ssj4eyewhite.png"), ctx.furDir(), ctx.genderDir(), ctx.eyeType()));
-        SSJ4_FACE.add(Eyebrows, Fur, (ctx) -> format(HD("%s/%s/face_%s/ssj4brows.png"), ctx.furDir(), ctx.genderDir(), ctx.eyeType()));
-        SSJ4_FACE.add(Eyebrows, Hair, (ctx) -> format(HD("%s/%s/face_%s/ssj4brows2.png"), ctx.furDir(), ctx.genderDir(), ctx.eyeType()));
-        SSJ4_FACE.add(LeftEye, Eye, (ctx) -> format(HD("%s/%s/face_%s/ssj4eyeleft.png"), ctx.furDir(), ctx.genderDir(), ctx.eyeType()));
-        SSJ4_FACE.add(RightEye, Eye, (ctx) -> format(HD("%s/%s/face_%s/ssj4eyeright.png"), ctx.furDir(), ctx.genderDir(), ctx.eyeType()));
+        TexturePath SSJ4_PATH = (ctx, path) -> format("%s/%s/face_%s/%s.png", ctx.furDir(), ctx.genderDir(), ctx.eyeType(), path);
+        SSJ4_FACE.add(Face, BodyCM, ctx -> SSJ4_PATH.get(ctx, "ssj4shade.png"));
+        SSJ4_FACE.add(EyeWhite, 0xFFFFFF, ctx -> SSJ4_PATH.get(ctx, "ssj4eyewhite"));
+        SSJ4_FACE.add(Eyebrows, Fur, ctx -> SSJ4_PATH.get(ctx, "ssj4brows"));
+        SSJ4_FACE.add(Eyebrows, Hair, ctx -> SSJ4_PATH.get(ctx, "ssj4brows2"));
+        SSJ4_FACE.add(LeftEye, Eye, ctx -> SSJ4_PATH.get(ctx, "ssj4eyeleft"));
+        SSJ4_FACE.add(RightEye, Eye, ctx -> SSJ4_PATH.get(ctx, "ssj4eyeright"));
         SSJ4_FACE.disable(Part.Eyebrows, Part.EyeWhite, Part.LeftEye, Part.RightEye);
 
-        SSJ3_FACE.add(Face, BodyCM, (ctx) -> format(HD("ssj3/%s/face_%s/ssj3shade.png"), ctx.genderDir(), ctx.eyeType()));
-        SSJ3_FACE.add(EyeWhite, 0xFFFFFF, (ctx) -> format(HD("ssj3/%s/face_%s/ssj3eyewhite.png"), ctx.genderDir(), ctx.eyeType()));
-        SSJ3_FACE.add(Eyebrows, Fur, (ctx) -> format(HD("ssj3/%s/face_%s/ssj3brows.png"), ctx.genderDir(), ctx.eyeType()));
-        SSJ3_FACE.add(LeftEye, Eye, (ctx) -> format(HD("ssj3/%s/face_%s/ssj3eyeleft.png"), ctx.genderDir(), ctx.eyeType()));
-        SSJ3_FACE.add(RightEye, Eye, (ctx) -> format(HD("ssj3/%s/face_%s/ssj3eyeright.png"), ctx.genderDir(), ctx.eyeType()));
+
+        TexturePath SSJ3_PATH = (ctx, path) -> format("ssj3/%s/face_%s/%s.png", ctx.genderDir(), ctx.eyeType(), path);
+        SSJ3_FACE.add(Face, BodyCM, ctx -> SSJ3_PATH.get(ctx, "ssj3shade"));
+        SSJ3_FACE.add(EyeWhite, 0xFFFFFF, ctx -> SSJ3_PATH.get(ctx, "ssj3eyewhite"));
+        SSJ3_FACE.add(Eyebrows, Fur, ctx -> SSJ3_PATH.get(ctx, "ssj3brows"));
+        SSJ3_FACE.add(LeftEye, Eye, ctx -> SSJ3_PATH.get(ctx, "ssj3eyeleft"));
+        SSJ3_FACE.add(RightEye, Eye, ctx -> SSJ3_PATH.get(ctx, "ssj3eyeright"));
         SSJ3_FACE.disable(Part.Eyebrows, Part.EyeWhite, Part.LeftEye, Part.RightEye);
 
-        PUPILS.add(LeftEye, Eye, (ctx) -> OVERLAY_DIR + format("eyespupils/left/left_eye_%s.png", ctx.eyeType()));
-        PUPILS.add(RightEye, Eye, (ctx) -> OVERLAY_DIR + format("eyespupils/right/right_eye_%s.png", ctx.eyeType()));
+
+        TexturePath PUPILS_PATH = (ctx, path) -> OVERLAY_DIR + format("eyespupils/%s_%s.png", path, ctx.eyeType());
+        PUPILS.add(LeftEye, Eye, ctx -> PUPILS_PATH.get(ctx, "left/left_eye"));
+        PUPILS.add(RightEye, Eye, ctx -> PUPILS_PATH.get(ctx, "right/right_eye"));
         PUPILS.disable(Part.LeftEye, Part.RightEye);
+    }
+
+    public interface TexturePath {
+        String get(OverlayContext ctx, String path);
     }
 }
 /*
