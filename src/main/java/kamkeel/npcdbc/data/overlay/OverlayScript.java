03ff0f1ee3efead6c2aab1048f413409d2a472b5
bigguy345 <monkeybar3200@gmail.com>
2025-12-14 20:50:44 +0200

    Refactor

--- src/main/java/kamkeel/npcdbc/data/overlay/OverlayScript.java
+++ src/main/java/kamkeel/npcdbc/data/overlay/OverlayScript.java
@@ -3,21 +3,21 @@ package kamkeel.npcdbc.data.overlay;
 import io.github.somehussar.janinoloader.api.script.IScriptBodyBuilder;
 import kamkeel.npcdbc.client.utils.Color;
 
-public class OverlayScript extends JaninoScript<OverlayScript.OverlayFunctions> {
+public class OverlayScript extends JaninoScript<OverlayScript.Functions> {
 
     public OverlayScript() {
-        super(OverlayFunctions.class);
+        super(Functions.class);
     }
 
     @Override
-    protected void configure(IScriptBodyBuilder<OverlayFunctions> builder) {
+    protected void configure(IScriptBodyBuilder<Functions> builder) {
         builder.setDefaultImports(
                 "kamkeel.npcdbc.client.utils.Color",
                 "kamkeel.npcdbc.data.overlay.OverlayContext"
         );
     }
 
-    public static class OverlayFunctions implements Overlay.TextureFunction, Overlay.ColorFunction {
+    public static class Functions implements Overlay.TextureFunction, Overlay.ColorFunction {
         public Color getColor(OverlayContext ctx) {
             return null;
         }
