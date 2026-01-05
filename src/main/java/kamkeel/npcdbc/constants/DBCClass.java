2a5e39e8c7d57e11c044b02042ca038fc403f45e
bigguy345 <monkeybar3200@gmail.com>
2026-01-05 17:45:06 +0200

    revert test

--- src/main/java/kamkeel/npcdbc/constants/DBCClass.java
+++ src/main/java/kamkeel/npcdbc/constants/DBCClass.java
@@ -4,6 +4,4 @@ public class DBCClass {
     public static final int MartialArtist = 0;
     public static final int Spiritualist = 1;
     public static final int Warrior = 2;
-    public static final int FAAAAH = 2;
-    
 }
