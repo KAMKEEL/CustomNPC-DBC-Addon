b8781d6dda0b85cd72849f0bb921c5a87186bf1d
bigguy345 <monkeybar3200@gmail.com>
2026-01-14 02:49:21 +0200

    Passed default imports to JaninoScript

--- src/main/java/kamkeel/npcdbc/data/overlay/OverlayScript.java
+++ src/main/java/kamkeel/npcdbc/data/overlay/OverlayScript.java
@@ -5,9 +5,13 @@ import noppes.npcs.janino.JaninoScript;
 
 public class OverlayScript extends JaninoScript<IOverlayScript.Functions> implements IOverlayScript {
 
+    /** Default imports available to overlay scripts without explicit import statements. */
+    public static final String[] DEFAULT_IMPORTS = {
+        "kamkeel.npcdbc.api.Color",
+        "kamkeel.npcdbc.api.client.overlay.IOverlayContext"
+    };
+
     public OverlayScript() {
-        super(Functions.class, (builder) -> builder.setDefaultImports(
-                "kamkeel.npcdbc.api.Color",
-                "kamkeel.npcdbc.api.client.overlay.IOverlayContext"));
+        super(Functions.class, DEFAULT_IMPORTS);
     }
 }
