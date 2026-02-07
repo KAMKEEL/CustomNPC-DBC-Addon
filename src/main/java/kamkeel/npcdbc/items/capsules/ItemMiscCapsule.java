0d4253197fdc490e674e4ff2712841d4410ac8d5
lonewolf8351 <122708538+lonewolf8351@users.noreply.github.com>
2026-02-07 13:11:04 -0500

    fix: fixed no fuse capsule not catching null

--- src/main/java/kamkeel/npcdbc/items/capsules/ItemMiscCapsule.java
+++ src/main/java/kamkeel/npcdbc/items/capsules/ItemMiscCapsule.java
@@ -161,13 +161,13 @@ public class ItemMiscCapsule extends Item {
             }
 
         } else if (meta == EnumMiscCapsules.NoFuse.getMeta()) {
-            if (Integer.parseInt(player.getEntityData().getCompoundTag("PlayerPersisted").getString("jrmcFuzion")) <= 0) {
+            if (player.getEntityData().getCompoundTag("PlayerPersisted").getString("jrmcFuzion") == null || player.getEntityData().getCompoundTag("PlayerPersisted").getString("jrmcFuzion").matches(" ") ){
                 player.addChatComponentMessage(new ChatComponentText("§cYou do not have no fuse"));
                 return itemStack;
-            } else {
-                setInt(0, player, "jrmcFuzion");
-                player.addChatComponentMessage(new ChatComponentText("§aYou no longer have no fuse!"));
             }
+            setInt(0, player, "jrmcFuzion");
+            player.addChatComponentMessage(new ChatComponentText("§aYou no longer have no fuse!"));
+
 
         } else if (meta == EnumMiscCapsules.Exhausted.getMeta()) {
             if (!DBCEffectController.getInstance().hasEffect(player, Effects.EXHAUSTED)) {
