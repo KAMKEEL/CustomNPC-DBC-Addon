2552b6813814f17f18b2c57f0884ee50f6937cc6
Vidal <imnotputtingmyemailhereami@gmail.com>
2026-02-14 16:22:54 -0300

    Added an option to register multiple animations under one common name

--- src/main/java/kamkeel/npcdbc/constants/DBCAnimations.java
+++ src/main/java/kamkeel/npcdbc/constants/DBCAnimations.java
@@ -3,11 +3,14 @@ package kamkeel.npcdbc.constants;
 import kamkeel.npcdbc.CustomNpcPlusDBC;
 import kamkeel.npcs.util.Register;
 import noppes.npcs.controllers.data.Animation;
+import noppes.npcs.controllers.data.BuiltInAnimation;
 
 public class DBCAnimations {
     public static Register.Animations ANIMATIONS = Register.Animations.create(CustomNpcPlusDBC.class, "animations","npcdbc");
 
-    public static final Animation NAMEK_REGEN = ANIMATIONS.register("Namek_Regen", Animation::new);
+    public static final Animation NAMEK_REGEN = ANIMATIONS.register("Namek_Regen", BuiltInAnimation::new);
+    public static final Animation[] KAMEHAMEHA = ANIMATIONS.registerBundle("Kamehameha", BuiltInAnimation::new,
+        "Windup", "Charge", "Active");
 
     public static void register() {
         ANIMATIONS.register();
