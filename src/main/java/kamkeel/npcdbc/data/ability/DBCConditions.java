b7798327ad677f76dec625d0b62f52d0af1ee802
Kam <KamronGaming@gmail.com>
2026-02-21 02:36:26 -0500

    Condition Registry [1/2]

--- src/main/java/kamkeel/npcdbc/data/ability/DBCConditions.java
+++ src/main/java/kamkeel/npcdbc/data/ability/DBCConditions.java
@@ -4,15 +4,16 @@ import kamkeel.npcdbc.data.ability.conditions.ConditionKiThreshold;
 import kamkeel.npcdbc.data.ability.conditions.ConditionForm;
 import kamkeel.npcdbc.data.ability.conditions.ConditionRace;
 import kamkeel.npcs.controllers.AbilityController;
+import kamkeel.npcs.util.Register;
 
 public class DBCConditions {
     public static void register() {
         if (AbilityController.Instance == null) return;
 
-        AbilityController ctrl = AbilityController.Instance;
-
-        ctrl.registerCondition(ConditionKiThreshold::new);
-        ctrl.registerCondition(ConditionForm::new);
-        ctrl.registerCondition(ConditionRace::new);
+        Register.Conditions conditions = Register.Conditions.create("npcdbc", "DBC Addon");
+        conditions.register("ki_threshold", ConditionKiThreshold::new);
+        conditions.register("form", ConditionForm::new);
+        conditions.register("race", ConditionRace::new);
+        conditions.register();
     }
 }
