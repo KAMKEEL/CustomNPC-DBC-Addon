c1cd2204130e7c25dcbefb23fd7fb5cdce2fadc4
Kam <KamronGaming@gmail.com>
2026-02-24 15:33:30 -0500

    1.2 Beta 9

--- src/main/java/kamkeel/npcdbc/CustomNpcPlusDBC.java
+++ src/main/java/kamkeel/npcdbc/CustomNpcPlusDBC.java
@@ -45,7 +45,7 @@ public class CustomNpcPlusDBC {
 
     public static final String name = "CustomNPC+ DBC Addon";
     public static final String ID = "npcdbc";
-    public static final String version = "1.2-beta8";
+    public static final String version = "1.2-beta9";
 
     @SidedProxy(clientSide = "kamkeel.npcdbc.client.ClientProxy", serverSide = "kamkeel.npcdbc.CommonProxy")
     public static CommonProxy proxy;
