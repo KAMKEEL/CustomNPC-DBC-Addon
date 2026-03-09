b8c351096de39f1fe016859307b49851e7b0dcf0
somehussar <148253652+somehussar@users.noreply.github.com>
2026-03-09 23:29:53 +0100

    Why didnt this commit?

---
+++ .github/scripts/resolve-doc-path.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+MAIN_BRANCH_NAME="main"
+
+# -----------------------------------------------
+# WHITELIST: Add branch names here to give them
+# a page under experimental/<branch>
+# `main` is handled AUTOMATICALLY
+#
+# Notice how they do NOT have commas.
+# Gotta love bash. -Hussar
+# -----------------------------------------------
+EXPERIMENTAL_WHITELIST=(
+  "dev"
+  # "new-branch"
+)
+
+
+BRANCH="${GITHUB_REF#refs/heads/}"
+SAFE_BRANCH="${BRANCH//\//-}"
+
+# This branch is the logic for getting the proper `/releases/<version>/` path
+# It also provides `/releases/latest` by default for MAIN.
+if [ "$BRANCH" = "$MAIN_BRANCH_NAME" ]; then
+
+  # For anyone having to deal with this bash script later on. This is the current version checking order
+  # -> check if gradle.properties has a `modVersion` tag. If it does, just grab it.
+  # -> check if it has the `version` tag. Grab it if it does
+  # -> check build.gradle for the version tag.
+  if [ -f "gradle.properties" ] && grep -q "^modVersion=" gradle.properties; then
+    VERSION=$(grep "^modVersion=" gradle.properties | cut -d'=' -f2 | tr -d '[:space:]')
+  elif [ -f "gradle.properties" ] && grep -q "^version=" gradle.properties; then
+    VERSION=$(grep "^version=" gradle.properties | cut -d'=' -f2 | tr -d '[:space:]')
+  elif [ -f "build.gradle" ]; then
+    VERSION=$(grep "^version" build.gradle | head -1 | sed "s/version\s*=\s*['\"]//;s/['\"].*//;s/\s//g")
+  elif [ -f "build.gradle.kts" ]; then
+    VERSION=$(grep "^version" build.gradle.kts | head -1 | sed "s/version\s*=\s*\"//;s/\".*//;s/\s//g")
+  else
+    echo "Could not find version. Defaulting to 'unknown'."
+    VERSION="unknown"
+  fi
+  echo "Resolved version: $VERSION"
+
+  # Storing the output for use in the action GH Workflow script.
+  echo "path=releases/latest" >> $GITHUB_OUTPUT
+  echo "extra_path=releases/$VERSION" >> $GITHUB_OUTPUT
+
