a8afb16eab8f7ce6b045b2dcd2a84e0820cbf1eb
bigguy345 <monkeybar3200@gmail.com>
2026-03-28 20:42:12 +0200

    dawg dude bro

--- src/main/java/kamkeel/npcdbc/data/dbcdata/DBCData.java
+++ src/main/java/kamkeel/npcdbc/data/dbcdata/DBCData.java
@@ -609,10 +609,8 @@ public class DBCData extends DBCDataUniversal implements IAuraData {
     }
 
     public HashMap<Integer, String> getUnlockedDBCFormsMap() {
-        if (addonRace.isCustomRace()) return new LinkedHashMap<>();
-
         HashMap<Integer, String> dbcForms = new LinkedHashMap<>();
-        int race = Race;
+        int race = addonRace.getRaceID() == -1 ? Race : -1;
         int racialSkill = JRMCoreH.SklLvlX(1, RacialSkills) - 1;
         int godSkill = JRMCoreH.SklLvl(9);
 
