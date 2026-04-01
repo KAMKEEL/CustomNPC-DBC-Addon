40ee0aeca2e728da71d1d76a7adb327c1f85f9dc
bigguy345 <monkeybar3200@gmail.com>
2026-04-02 00:52:04 +0200

    No longer needed DBC addon PlayerData updateClient (handled by CNPC+)

--- src/main/java/kamkeel/npcdbc/ServerEventHandler.java
+++ src/main/java/kamkeel/npcdbc/ServerEventHandler.java
@@ -23,16 +23,13 @@ import kamkeel.npcdbc.data.form.Form;
 import kamkeel.npcdbc.data.npc.DBCDisplay;
 import kamkeel.npcdbc.entity.EntityAura;
 import kamkeel.npcdbc.mixins.late.INPCDisplay;
-import kamkeel.npcdbc.mixins.late.IPlayerDBCInfo;
 import kamkeel.npcdbc.network.DBCPacketHandler;
 import kamkeel.npcdbc.network.packets.get.CapsuleInfo;
-import kamkeel.npcdbc.network.packets.get.DBCInfoSyncPacket;
 import kamkeel.npcdbc.network.packets.player.LoginInfo;
 import kamkeel.npcdbc.data.ability.DBCAbilities;
 import kamkeel.npcdbc.util.DBCUtils;
 import kamkeel.npcdbc.util.PlayerDataUtil;
 import kamkeel.npcdbc.util.Utility;
-import kamkeel.npcs.network.enums.EnumSyncAction;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -104,16 +101,6 @@ public class ServerEventHandler {
 
         EntityPlayer player = event.player;
         if (event.side == Side.SERVER && event.phase == TickEvent.Phase.START) {
-            // Send Form Information
-            if (PlayerDataController.Instance != null) {
-                PlayerData playerData = PlayerDataController.Instance.getPlayerData(player);
-                if (((IPlayerDBCInfo) playerData).getDBCInfoUpdate()) {
-                    NBTTagCompound formCompound = new NBTTagCompound();
-                    playerData.getDBCSync(formCompound);
-                    DBCPacketHandler.Instance.sendToPlayer(new DBCInfoSyncPacket(DBCSyncType.PLAYERDATA, EnumSyncAction.RELOAD, -1, formCompound), (EntityPlayerMP) player);
-                    ((IPlayerDBCInfo) playerData).endDBCInfo();
-                }
-            }
 
             if (ConfigDBCEffects.AUTO_BLOATED)
                 if (player.ticksExisted % ConfigDBCEffects.DECREASE_TIME == 0)
--- src/main/java/kamkeel/npcdbc/mixins/late/impl/npc/MixinPlayerData.java
+++ src/main/java/kamkeel/npcdbc/mixins/late/impl/npc/MixinPlayerData.java
@@ -4,38 +4,26 @@ import kamkeel.npcdbc.data.PlayerDBCInfo;
 import kamkeel.npcdbc.mixins.late.IPlayerDBCInfo;
 import noppes.npcs.controllers.data.PlayerData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 
 @Mixin(PlayerData.class)
 public abstract class MixinPlayerData implements IPlayerDBCInfo {
 
+    @Shadow
+    public boolean updateClient;
     @Unique
     public PlayerDBCInfo formData = new PlayerDBCInfo((PlayerData) (Object) this);
-
-    @Unique
-    public boolean formUpdate = false;
 
     @Unique
     public PlayerDBCInfo getPlayerDBCInfo() {
         return formData;
     }
-
-    @Unique
-    @Override
-    public boolean getDBCInfoUpdate() {
-        return formUpdate;
-    }
-
+    
     @Unique
     @Override
     public void updateDBCInfo() {
-        formUpdate = true;
-    }
-
-    @Unique
-    @Override
-    public void endDBCInfo() {
-        formUpdate = false;
+        this.updateClient = true;
     }
 }
